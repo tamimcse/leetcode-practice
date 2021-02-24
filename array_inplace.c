50afa5c6e1a5b78cfe6556b19b34a78e3708ff9a
tamimcse <tamim@csebuet.org>
2021-02-24 14:06:04 -0500

    .

---
+++ array_inplace.c
@@ -0,0 +1,41 @@
+//#include <stdlib.h>
+#include <stdio.h>
+//#include <stdbool.h>
+
+#define SIZE 11
+
+void swap (int *a, int *b)
+{
+  int tmp;
+  
+  tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+void inplace (int a[], int size)
+{
+  int i;
+
+  for (i = 0; i < size/2; i++) {
+    swap (&a[i], &a[size - 1 - i]);
+  }
+}
+
+void print (int a[], int size)
+{
+  int i;
+
+  for (i = 0; i < size; i++) {
+    printf ("%d ", a[i]);
+  }
+  printf ("\n");
+}
+
+void main () {
+  int a[SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+  print (a, SIZE);
+  inplace (a, SIZE);
+  print (a, SIZE);
+}
