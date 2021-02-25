c96aa74ce4ca9cc1cc5e9f0ed30c1100e63026a3
tamimcse <tamim@csebuet.org>
2021-02-24 20:57:59 -0500

    .

---
+++ reverse_list1.c
@@ -0,0 +1,64 @@
+/*
+Reverse a linked list
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int data;
+  struct node *next;
+} *head = NULL;
+
+void insert (int val)
+{
+  struct node *new_node = (struct node *)malloc (sizeof (*new_node));
+  new_node->data = val;
+  if (!head) {
+    head = new_node;
+  } else {
+    new_node->next = head;
+    head = new_node;
+  }
+}
+
+void print ()
+{
+  struct node *runner = head;
+
+  while (runner) {
+    printf ("%d ", runner->data);
+    runner = runner->next;
+  }
+  printf ("\n");
+}
+
+void reverse ()
+{
+  //the list is empty or have just one item
+  if (!head || !head->next) {
+    return;
+  }
+
+  struct node *runner1, *runner2, *nxt_runner2;
+  runner1 = head;
+  runner2 = head->next;
+  runner1->next = NULL;
+  while (runner2) {
+    nxt_runner2 = runner2->next;
+    runner2->next = runner1;
+    runner1 = runner2;
+    runner2 = nxt_runner2;
+  }
+  head = runner1;
+}
+
+void main ()
+{
+  insert (3);
+  insert (5);
+  insert (8);
+  insert (10);
+  print ();
+  reverse ();
+  print ();
+}
---
+++ reverse_list2.c
@@ -0,0 +1,68 @@
+/*
+Reverse a linked list.
+
+Implement reverse_list1.c first. Then change each function to take struct node **head.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int data;
+  struct node *next;
+};
+
+void insert (struct node **head, int val)
+{
+  struct node *new_node = (struct node *)malloc (sizeof (*new_node));
+  new_node->data = val;
+  if (!*head) {
+    *head = new_node;
+  } else {
+    new_node->next = *head;
+    *head = new_node;
+  }
+}
+
+static void print (struct node *head)
+{
+  struct node *runner = head;
+
+  while (runner) {
+    printf ("%d ", runner->data);
+    runner = runner->next;
+  }
+  printf ("\n");
+}
+
+void reverse (struct node **head)
+{
+  //the list is empty or have just one item
+  if (!*head || !(*head)->next) {
+    return;
+  }
+
+  struct node *runner1, *runner2, *nxt_runner2;
+  runner1 = *head;
+  runner2 = (*head)->next;
+  runner1->next = NULL;
+  while (runner2) {
+    nxt_runner2 = runner2->next;
+    runner2->next = runner1;
+    runner1 = runner2;
+    runner2 = nxt_runner2;
+  }
+  *head = runner1;
+}
+
+void main ()
+{
+  struct node *head = NULL;
+
+  insert (&head, 3);
+  insert (&head, 5);
+  insert (&head, 8);
+  insert (&head, 10);
+  print (head);
+  reverse (&head);
+  print (head);
+}
