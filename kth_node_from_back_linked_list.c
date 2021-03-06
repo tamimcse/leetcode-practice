ffa3d1ee4e62a3e2435b10e6671b8c45ad5ff03e
tamimcse <tamim@csebuet.org>
2021-03-06 14:48:08 -0500

    .

--- kth_node_from_back_linked_list.c
+++ kth_node_from_back_linked_list.c
@@ -17,19 +17,35 @@ void insert (struct node **head, int val)
   *head = new_node;
 }
 
+struct node * find_kth_elemement_from_back (struct node *head, int k)
+{
+  struct node *runner1 = head, *runner2 = NULL;
+  int i = 0;
+
+  while (runner1) {
+    ++i;
+    if (i == k) {
+      runner2 = head;
+    } else if (i > k) {
+      runner2 = runner2->next;
+    }
+    runner1 = runner1->next;
+  }
+  return runner2;
+}
 
 void main ()
 {
   struct node *head = NULL;
+  int k = 3;
 
   insert (&head, 10);
   insert (&head, 20);
   insert (&head, 30);
   insert (&head, 40);
   insert (&head, 50);
-  delete (&head, 40);
   insert (&head, 40);
   insert (&head, 50);
-  delete (&head, 40);
-  print (head);
+  struct node *res = find_kth_elemement_from_back (head, k);
+  printf ("%d-th element from back is %d\n", k, res ? res->data : 0);
 }
