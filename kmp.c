4bf55f1aaf034432d4c4fc66d2ce8a041e816aee
tamimcse <tamim@csebuet.org>
2021-03-14 15:53:40 -0400

    .

--- kmp.c
+++ kmp.c
@@ -4,24 +4,25 @@ String matching using KMP algorithm
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-  //calculate the longest prefix length which is also a suffix of the subpattern pattern. 
+  //calculate the longest proper prefix length which is also a suffix of the subpattern pattern. 
   //https://www.educative.io/edpresso/what-is-the-knuth-morris-pratt-algorithm
 int max_prex_len (char *str, int len)
 {
-  int prefix_len = 0, i, l;
+  int i, l;
 
-  for (l = 1; l <= len; l++) {
+  for (l = len - 1; l >= 1; l--) {
     for (i = 0; i < l; i++) {
-      if (str[i] != str[len - l + i])
+      if (str[i] != str[len - l + i]) {
         break;
+      }
     }
     if (i == l) {
-      prefix_len = l;
+      return l;
     }
   }
-  printf ("len = %d prefix_len = %d \n", len, prefix_len);
-  return prefix_len;
+  return 0;
 }
 
 void kmp (char *str, char *match)
@@ -31,15 +32,28 @@ void kmp (char *str, char *match)
   int match_len = strlen (match);
   int *prefix_lens = (int *) calloc (match_len, sizeof (*prefix_lens));
 
-  for (i = 0; i < match_len; i++) {
-    prefix_lens[i] = max_prex_len (match, i);
+  i = 0;
+  j = 0;
+  while (i < str_len) {
+    if (str[i] == match[j]) {
+      i++;
+      j++;
+      if (j == match_len) {
+        printf ("Match found at index %d !!! \n", i - match_len);
+      }
+      j = prefix_lens[match_len - 1];
+    } else {
+      
+    }
+    
   }
 
-  printf ("Array = ");
-  for (i = 0; i < match_len; i++)
-    printf ("%d ", prefix_lens[i]);
-  printf ("\n");
-  
+/*
+  for (i = 0; i < match_len; i++) {
+    prefix_lens[i] = max_prex_len (match, i + 1);
+    printf("prefix_lens[%d] = %d\n", i, prefix_lens[i]);
+  }
+*/  
   free (prefix_lens);
 }
 
