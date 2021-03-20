7f40d20f42e617fd51b4474322d784ab88d59fda
tamimcse <tamim@csebuet.org>
2021-03-20 14:42:23 -0400

    .

---
+++ reverse_double_linkedlist.c
@@ -0,0 +1,38 @@
+/*
+https://www.geeksforgeeks.org/reverse-a-doubly-linked-list/
+*/
+#include <stdio.h>
+
+struct node {
+  int data;
+  struct node *next;
+  struct node *prev;
+};
+
+void print (struct node *head)
+{
+  while (head) {
+    printf ("%d ", head->data);
+    head = head->next;
+  }
+  printf ("\n");
+}
+
+void reverse_double_ll (struct node **head)
+{
+
+}
+
+void main ()
+{
+  struct node n1, n2, n3;
+  n1 = (struct node) {1, &n2, NULL};
+  n2 = (struct node) {2, &n3, &n1};
+  n3 = (struct node) {3, NULL, &n2};
+
+  struct node *head = &n1;
+
+  print (head);
+  reverse_double_ll (&head);
+  print (head);
+}
