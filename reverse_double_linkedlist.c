1b58248d1443112a64bc113b156cb4dc4744d549
tamimcse <tamim@csebuet.org>
2021-03-20 15:54:06 -0400

    .

--- reverse_double_linkedlist.c
+++ reverse_double_linkedlist.c
@@ -20,21 +20,29 @@ void print (struct node *head)
 
 void reverse_double_ll (struct node **head)
 {
-  struct node *runner1 = *head, *runner2 = *head, *runner3;
+  struct node *runner1 = NULL, *runner2 = *head, *runner3;
   if (runner2)
     runner2 = runner2->next;
 
   while (runner2) {
-
+    runner1 = runner2->prev;
+    runner3 = runner2->next;
+    runner2->next = runner1;
+    runner1->prev = runner2;
+    runner2 = runner3;
   }
+  (*head)->next = NULL;
+  *head = runner1->prev;
 }
 
 void main ()
 {
-  struct node n1, n2, n3;
+  struct node n1, n2, n3, n4, n5;
   n1 = (struct node) {1, &n2, NULL};
   n2 = (struct node) {2, &n3, &n1};
-  n3 = (struct node) {3, NULL, &n2};
+  n3 = (struct node) {3, &n4, &n2};
+  n4 = (struct node) {4, &n5, &n3};
+  n5 = (struct node) {5, NULL, &n4};
 
   struct node *head = &n1;
 
