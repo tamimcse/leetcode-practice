fe80114da5e7e19fc27a6135181f1e998147106a
tamimcse <tamim@csebuet.org>
2021-03-22 17:07:51 -0400

    .

--- puzzle_3x3_matrix.c
+++ puzzle_3x3_matrix.c
@@ -10,6 +10,7 @@ Find the minimum number of exchanges needed to reach the final(goal) matrix.
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 bool is_prime (int a)
 {
@@ -31,6 +32,9 @@ struct state {
   int num_vertices;
   int num_edges;
   struct edge edges[12];
+  struct state *prev_state;
+  int swap_v1;
+  int swap_v2;
 };
 
 void print_state (struct state *s)
@@ -50,7 +54,7 @@ struct state *create_next_state (struct state *s, int v1, int v2)
   struct state *new_state = (struct state *) calloc (1, sizeof (*new_state));
   new_state->num_vertices = s->num_vertices;
   new_state->num_edges = s->num_edges;
-  new_state->edges = s->edges;
+  memcpy (new_state->edges, s->edges, sizeof (new_state->edges));
   for (i = 0; i < new_state->num_edges; i++) {
     if (new_state->edges[i].v1 == v1 || new_state->edges[i].v1 == v2) {
       new_state->edges[i].v1 = new_state->edges[i].v1 == v1 ? v2 : v1;
@@ -59,6 +63,9 @@ struct state *create_next_state (struct state *s, int v1, int v2)
       new_state->edges[i].v2 = new_state->edges[i].v2 == v1 ? v2 : v1;
     }
   }
+  new_state->prev_state = s;
+  new_state->swap_v1 = v1;
+  new_state->swap_v2 = v2;
   return new_state;
 }
 
@@ -67,10 +74,24 @@ bool check_target (struct state *s)
 {
   return ((s->edges[0].v1 == 1 || s->edges[0].v1 == 2) &&
           (s->edges[0].v2 == 1 || s->edges[0].v2 == 2) &&
+	  (s->edges[1].v1 == 1 || s->edges[1].v1 == 4) &&
+          (s->edges[1].v2 == 1 || s->edges[1].v2 == 4) &&
+	  (s->edges[2].v1 == 2 || s->edges[2].v1 == 3) &&
+          (s->edges[2].v2 == 2 || s->edges[2].v2 == 3) &&
+	  (s->edges[3].v1 == 2 || s->edges[3].v1 == 5) &&
+          (s->edges[3].v2 == 2 || s->edges[3].v2 == 5) &&
           (s->edges[4].v1 == 3 || s->edges[4].v1 == 6) &&
           (s->edges[4].v2 == 3 || s->edges[4].v2 == 6) &&
           (s->edges[5].v1 == 4 || s->edges[5].v1 == 5) &&
           (s->edges[5].v2 == 4 || s->edges[5].v2 == 5) &&
+          (s->edges[6].v1 == 4 || s->edges[6].v1 == 7) &&
+          (s->edges[6].v2 == 4 || s->edges[6].v2 == 7) &&
+          (s->edges[7].v1 == 5 || s->edges[7].v1 == 6) &&
+          (s->edges[7].v2 == 5 || s->edges[7].v2 == 6) &&
+          (s->edges[8].v1 == 5 || s->edges[8].v1 == 8) &&
+          (s->edges[8].v2 == 5 || s->edges[8].v2 == 8) &&
+          (s->edges[9].v1 == 6 || s->edges[9].v1 == 9) &&
+          (s->edges[9].v2 == 6 || s->edges[9].v2 == 9) &&
           (s->edges[10].v1 == 7 || s->edges[10].v1 == 8) &&
           (s->edges[10].v2 == 7 || s->edges[10].v2 == 8) &&
           (s->edges[11].v1 == 8 || s->edges[11].v1 == 9) &&
@@ -87,19 +108,32 @@ void solve_puzzle (struct state *start_state)
   queue [end++] = start_state;
   while (end > start) {
     curr_state = queue[start++];
+    printf ("current state = \n");
+    print_state (curr_state);
     for (i = 0; i < curr_state->num_edges; i++) {
-      if (is_prime(curr_state->edges[i].v1 + curr_state->edges[i].v2)) {
-        nxt_state = create_next_state (curr_state);
+      int v1 = curr_state->edges[i].v1;
+      int v2 = curr_state->edges[i].v2;
+//      printf ("processing node %d and %d !!!!!!\n", v1, v2);
+      if (is_prime(v1 + v2)) {
+        nxt_state = create_next_state (curr_state, v1, v2);
         if (check_target (nxt_state)) {
           printf ("target is reached !!!!!!\n");
-          return;
+          goto finish;
         } else {
+          printf ("swapping %d and %d \n", v1, v2);
           queue [end++] = nxt_state;
         }
       }
     }
   }
   printf ("Target state is not reachable \n");
+finish:
+  curr_state = nxt_state;
+  while (curr_state != start_state) {
+    printf ("(%d, %d) ", curr_state->swap_v1, curr_state->swap_v2);
+    curr_state = curr_state->prev_state;
+  }
+  printf ("\n");
 }
 
 void main()
@@ -125,6 +159,8 @@ void main()
 
   printf ("row = %d \n", row);
 
-  print_state (&s);
+//  print_state (&s);
+
+  solve_puzzle (&s);
 }
 
