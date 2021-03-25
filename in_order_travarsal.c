549f99f827ff2f383cfcc6454318f02dd27234fb
tamimcse <tamim@csebuet.org>
2021-03-24 21:05:44 -0400

    .

--- in_order_travarsal.c
+++ in_order_travarsal.c
@@ -18,6 +18,36 @@ void inorder1(struct node *r)
   inorder1 (r->right);
 }
 
+void inorder2(struct node *r)
+{
+  struct node *stack[100];
+  struct node *curr;
+  int start = 0;
+
+  if (!r)
+    return;
+
+  int backtracking = 0;
+  stack[start++] = r;
+  while (start) { 
+    curr = stack[start - 1];
+    if (!backtracking && curr->left) {
+      stack[start++] = curr->left;
+      //going deep
+      backtracking = 0;
+    } else {
+      backtracking = 1;
+      curr = stack[--start];
+      printf ("%d ", curr->data);
+      if (curr->right) {
+        stack[start++] = curr->right;
+        //going deep
+        backtracking = 0;
+      }
+    }
+  }
+}
+
 void main ()
 {
   struct node n1, n2, n3, n4, n5, n6, n7, n8, n9, n10;
@@ -33,6 +63,9 @@ void main ()
   n10 = (struct node) {10, NULL, NULL};
 
   printf ("In order travarsal (recursive) = ");
+  inorder1 (&n1);
+  printf ("\n");
+  printf ("In order travarsal (non recursive) = ");
   inorder1 (&n1);
   printf ("\n");
 }
