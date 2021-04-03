8d586eb8b18aea44e8c3e9d075bebacbdf2cae52
tamimcse <tamim@csebuet.org>
2021-04-03 16:51:21 -0400

    .

--- matrix-chain-multiplication.c
+++ matrix-chain-multiplication.c
@@ -3,10 +3,11 @@ https://www.geeksforgeeks.org/matrix-chain-multiplication-dp-8/
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 void matrix_chain_multiplication (int arr[], int num_matrix)
 {
-  int i, j, row, col, res1, res2;
+  int i, j, row, col, res1, res2, res, k, min_res, m;
   int *dp_arr = (int *) calloc (num_matrix * num_matrix, sizeof (*dp_arr));
 
   for (i = 0; i < num_matrix; i++) {
@@ -15,12 +16,17 @@ void matrix_chain_multiplication (int arr[], int num_matrix)
 
   for (i = 1; i < num_matrix; i++) {
     for (col = i, row = 0; col < num_matrix; col++, row++) {
-      res1 = (arr[row] * arr[col] * arr[col + 1]) + dp_arr[row * num_matrix + col - 1];
-      res2 = (arr[row] * arr[row + 1] * arr[col + 1]) + dp_arr[(row + 1) * num_matrix + col];
-      dp_arr[row * num_matrix + col] = res1 < res2 ? res1 : res2;
+      min_res = INT_MAX;
+      for (k = row, m = 1; k < col; k++, m++) {
+        res = dp_arr[row * num_matrix + k] + dp_arr[(row + m) * num_matrix + col] + arr[row] * arr[k + 1] * arr[col + 1];
+        //printf ("res = %d\n", res);
+        if (res < min_res)
+          min_res = res;
+      }
+      dp_arr[row * num_matrix + col] = min_res;
       //printf ("(%d, %d) = %d ", row, col, dp_arr[row * num_matrix + col]);
     }
-    //printf ("\n");
+    printf ("\n");
   }
 
   printf ("Value of result is %d \n", dp_arr[num_matrix - 1]);
