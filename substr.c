63ce5e3786bf0576d15b0de3b91b5af020849b19
tamimcse <tamim@csebuet.org>
2021-04-05 18:36:10 -0400

    .

--- substr.c
+++ substr.c
@@ -4,6 +4,7 @@ https://www.geeksforgeeks.org/program-print-substrings-given-string/
 #include <stdio.h>
 #include <string.h>
 
+//It generates same substr multiple times (uses backtracking)
 void substr (char *str, int start, int end)
 {
   int i;
@@ -27,10 +28,28 @@ void substr (char *str, int start, int end)
   str[end] = tmp;
 }
 
+void substr_itr (char *str, int len)
+{
+  int i, j, k;
+
+  for (i = 0; i < len; i++) {
+    for (j = len - 1; j >= 0; j--) {
+      for (k = i; k <= j; k++) {
+        printf ("%c", str[k]);
+      }
+      printf ("\n");
+    }
+  }
+}
+
 void main ()
 {
   char str[] = "abcd";
   int len = strlen (str);
 
+  printf ("substring backtracking version:\n");
   substr (str, 0, len - 1);
+
+  printf ("substring iterative version:\n");
+  substr_itr (str, len);
 }
