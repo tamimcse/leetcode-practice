51514ad73dbf8c7a824ec5f750fb2439923251cd
tamimcse <tamim@csebuet.org>
2021-04-09 16:47:12 -0400

    .

--- longest-substring-without-repeating-characters.c
+++ longest-substring-without-repeating-characters.c
@@ -3,25 +3,36 @@ https://www.geeksforgeeks.org/length-of-the-longest-substring-without-repeating-
 */
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int len_of_longest_substr_without_repeating_chars(char *str)
 {
-  int i;
-  char hash[256] = {0};
-  int count = 0;
+  int i, j, count, max_substr_len = 0;
+  int len = strlen (str);
+  int *dp = (int *) calloc (len, sizeof (*dp));
 
-  for (i = 0; str[i]; i++) {
-    if (!hash[str[i]]) {
-      hash[str[i]] = 1;
-      count++;
+  dp [0] = 1;
+  for (i = 1; str[i]; i++) {
+    count = 1;
+    for (j = i - 1; j >= 0; j--) {
+      if (str[i] != str[j])
+        count++;
+      else
+        break;
     }
+    dp[i] = count;
   }
-  return count;
+
+  for (i = 0; i < len; i++) {
+    if (dp[i] > max_substr_len)
+      max_substr_len = dp[i];
+  }
+  return max_substr_len;
 }
 
 void main ()
 {
-  char arr[] = "ABDEFGABEF";
+  char arr[] = "ilovecincinati";
   int res = len_of_longest_substr_without_repeating_chars (arr);
   printf ("Result is %d \n", res);
 }
