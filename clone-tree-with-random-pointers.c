cd59f0503b2ff3b36588362c1e2993e53e4a3fe3
tamimcse <tamim@csebuet.org>
2021-04-12 13:37:13 -0400

    .

--- clone-tree-with-random-pointers.c
+++ clone-tree-with-random-pointers.c
@@ -13,17 +13,57 @@ struct node {
   struct node *random;
 };
 
-struct node* clone (struct node *r)
+struct node_map {
+  struct node *key;
+  struct node *value;
+};
+
+struct node* clone_inner (struct node *r, struct node_map *map, int *map_count)
 {
   struct node *left = NULL, *right = NULL, *new_node;
   if (r->left)
-    left = clone (r->left);
+    left = clone_inner (r->left, map, map_count);
   if (r->right)
-    right = clone (r->right);
+    right = clone_inner (r->right, map, map_count);
   new_node = (struct node*) malloc (sizeof (*new_node));
   new_node->data = r->data;
   new_node->left = left;
   new_node->right = right;
+  map[*map_count].key = r;
+  map[*map_count].value = new_node;
+  (*map_count)++;
+  return new_node;
+}
+
+struct node* find_value (struct node_map *map, int map_count, struct node *key)
+{
+  int i;
+
+  for (i = 0; i < map_count; i++) {
+    if (map[i].key == key)
+      return map[i].value;
+  }
+  return NULL;
+}
+
+void set_random_pointer (struct node *r, struct node_map *map, int map_count)
+{
+  if (r->random)
+    find_value(map, map_count, r)->random = find_value(map, map_count, r->random);
+  if (r->left)
+    set_random_pointer (r->left, map, map_count);
+  if (r->right)
+    set_random_pointer (r->right, map, map_count);
+}
+
+struct node* clone (struct node *r)
+{
+  struct node_map map[10] = {0};
+  int map_count = 0;
+
+  struct node *res = clone_inner (r, map, &map_count);
+  set_random_pointer (r, map, map_count);
+  return res;
 }
 
 void print (struct node *r)
