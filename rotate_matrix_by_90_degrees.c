41172f5e8d4c27f0a0bb39dd117808ae3df93b71
tamimcse <tamim@csebuet.org>
2021-04-13 16:17:27 -0400

    .

--- rotate_matrix_by_90_degrees.c
+++ rotate_matrix_by_90_degrees.c
@@ -4,6 +4,7 @@ https://www.geeksforgeeks.org/turn-an-image-by-90-degree/
 https://www.geeksforgeeks.org/inplace-rotate-square-matrix-by-90-degrees/
 */
 #include <stdio.h>
+#include <stdlib.h>
 
 int* rotate (int *mat, int N)
 {
@@ -12,13 +13,12 @@ int* rotate (int *mat, int N)
 
   for (i = 0; i < N; i++) {
     for (j = 0; j < N; j++) {
-      extra = (i + N - 1) % N;
-      m = (i + N - 1) >= N ? (i + N - 1);
-      n = j + extra;
-      res [m * N + n] = mat[i * N + j]; 
+      m = N - 1 - j;
+      n = i;
+      res[m * N + n] = mat[i * N + j];
     }
   }
-  return res;
+  return res; 
 }
 
 void print (int *mat, int N)
@@ -34,11 +34,23 @@ void print (int *mat, int N)
 
 void main ()
 {
-  int mat[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+  int N = 4;
+  int *mat = (int *)malloc(N * N * sizeof (*mat));
   int *res;
+  int i, j, cur;
 
-  print (mat, 4);
-  res = rotate (mat, 4);
-  print (res, 4);
+  for (i = 0, cur = 1; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      mat[i * N + j] = cur++;
+    }
+  }  
+
+  printf ("Original matrix = \n");
+  print (mat, N);
+
+  res = rotate (mat, N);
+
+  printf ("Result matrix = \n");
+  print (res, N);
 }
 
