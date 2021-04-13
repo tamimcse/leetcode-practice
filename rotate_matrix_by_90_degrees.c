0320a8621e169317b5ad2674659752f6932fe1d9
tamimcse <tamim@csebuet.org>
2021-04-13 15:29:49 -0400

    .

--- rotate_matrix_by_90_degrees.c
+++ rotate_matrix_by_90_degrees.c
@@ -1,7 +1,25 @@
 /*
+https://www.geeksforgeeks.org/turn-an-image-by-90-degree/
+
 https://www.geeksforgeeks.org/inplace-rotate-square-matrix-by-90-degrees/
 */
 #include <stdio.h>
+
+int* rotate (int *mat, int N)
+{
+  int i, j, m, n, extra;
+  int *res = (int *) calloc (N * N, sizeof (*res));
+
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      extra = (i + N - 1) % N;
+      m = (i + N - 1) >= N ? (i + N - 1);
+      n = j + extra;
+      res [m * N + n] = mat[i * N + j]; 
+    }
+  }
+  return res;
+}
 
 void print (int *mat, int N)
 {
@@ -17,7 +35,10 @@ void print (int *mat, int N)
 void main ()
 {
   int mat[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+  int *res;
 
   print (mat, 4);
+  res = rotate (mat, 4);
+  print (res, 4);
 }
 
