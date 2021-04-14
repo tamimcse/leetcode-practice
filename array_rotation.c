6a2f34cbf95888feca9ea76df5eabacddae643dd
tamimcse <tamim@csebuet.org>
2021-04-14 16:29:41 -0400

    .

---
+++ array_rotation.c
@@ -0,0 +1,17 @@
+/*
+https://www.geeksforgeeks.org/array-rotation/
+
+Too simple
+*/
+#include <stdio.h>
+
+void rotate (int *arr, int arr_len, int rotate_count)
+{
+  int *tmp = (int *)
+}
+
+void main ()
+{
+  int arr[] = {1, 2, 3, 4, 5, 6, 7};
+  int arr_len = sizeof (arr) / sizeof (arr[0]);
+}
