23a2e32587ec503c29a4ac22d1095bb93a758f5d
tamimcse <tamim@csebuet.org>
2021-04-15 13:06:11 -0400

    .

---
+++ all-possible-palindome-partition.c
@@ -0,0 +1,7 @@
+/*
+https://www.geeksforgeeks.org/given-a-string-print-all-possible-palindromic-partition/
+
+Use backtracking to check each substring to see if it's a palindrome. Simple, so not implementing it now.
+
+*/
+
