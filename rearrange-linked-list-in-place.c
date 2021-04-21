29e932ebb21bd6805c70db6380f2613521d6b706
tamimcse <tamim@csebuet.org>
2021-04-21 19:49:08 -0400

    .

---
+++ rearrange-linked-list-in-place.c
@@ -0,0 +1,7 @@
+/*
+https://www.geeksforgeeks.org/rearrange-a-given-linked-list-in-place/
+
+Simple. 
+
+Store the node pointers in a stack. Now iterate through the beginning of the linkeded list. For each element, pop from stack and insert it. Remove the bottom of the stack as well.
+*/
