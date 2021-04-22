7b0c4896e8f1c44c2287d4e53928f16aa579dad4
tamimcse <tamim@csebuet.org>
2021-04-21 20:34:04 -0400

    .

---
+++ sum-and-count.c
@@ -0,0 +1,13 @@
+/*
+Given Sum and Count. You can use the only integer from 1-9 (can use more than one time) to make the Sum. The restriction is that the number of the element should be equal to Count. Find all possible combinations.
+Example:
+Sum = 10 and Count = 2
+Result is (1, 9), (2, 8), (3, 7), (4, 6) and (5, 5)
+1 + 9 = 10
+2 + 8 = 10
+3 + 7 = 10
+4 + 6 = 10
+5 + 5 = 10
+
+Use backtracking
+*/
