e7ef3ab041b197f68d31dac265ca533877f7512f
tamimcse <tamim@csebuet.org>
2021-04-21 20:12:19 -0400

    .

---
+++ remove-adjacent-duplicates.c
@@ -0,0 +1,10 @@
+/*
+Remove adjacent elements from circular linked list if they are same.
+
+     eg - Input - 1 1 2 2 3 4 1
+            Output- 1 2 3 4 
+
+Tricky part was that the last element is adjacent to first element in Circular Linked List
+
+Simple
+*/
