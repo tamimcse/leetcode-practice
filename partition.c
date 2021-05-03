ed2ccbd7537317343483aa2313c053da96946e4c
tamimcse <tamim@csebuet.org>
2021-05-02 21:26:13 -0400

    .

--- partition.c
+++ partition.c
@@ -1,5 +1,7 @@
 /*
 https://www.geeksforgeeks.org/partition-problem-dp-18/
 
+https://www.geeksforgeeks.org/partition-a-set-into-two-subsets-such-that-the-difference-of-subset-sums-is-minimum/
+
 Use subset sum (Backtracking)
 */
