c64fd73c7ccd47432bbc0dc8bb3578dc78493d50
tamimcse <tamim@csebuet.org>
2021-05-09 16:29:17 -0400

    .

---
+++ linked-list-cycle.c
@@ -0,0 +1,18 @@
+/*
+https://leetcode.com/problems/linked-list-cycle/
+*/
+//Use hashtable instead of stack
+bool hasCycle(struct ListNode *head) {
+  struct ListNode *stack[10000] = {0};
+  int idx = 0, i;
+  
+  while (head) {
+    for (i = 0; i < idx; i++) {
+      if (stack[i] == head)
+        return true;
+    }
+    stack[idx++] = head;
+    head = head->next;
+  }
+  return false;
+}
