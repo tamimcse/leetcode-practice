e4c3b50ff6a8960a3ac379a323c0d96a810866e0
tamimcse <tamim@csebuet.org>
2021-05-24 20:52:04 -0400

    .

--- subset.c
+++ subset.c
@@ -28,6 +28,38 @@ void subset (int *arr, int len)
   }
 }
 
+/*
+
+https://leetcode.com/problems/subsets/
+
+void subsets_inner (int* nums, int numsSize, int *state, int state_cnt, int start, int **res, int *res_num, int *returnColumnSizes)
+{
+  int i;
+  
+  for (i = start; i < numsSize; i++) {
+    state[state_cnt] = nums[i];
+    res[*res_num] = (int *) malloc ((state_cnt + 1) * sizeof (int));
+    memcpy (res[*res_num], state, (state_cnt + 1) * sizeof (int));
+    returnColumnSizes[*res_num] = state_cnt + 1;
+    (*res_num)++;
+    subsets_inner (nums, numsSize, state, state_cnt + 1, i + 1, res, res_num, returnColumnSizes);
+  }
+}
+
+int** subsets(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
+  int res_cnt = pow (2, numsSize);
+  *returnSize = res_cnt;
+  int **res = (int **) calloc (res_cnt, sizeof (*res));
+  int res_num = 0;
+  int *state = (int *) malloc (numsSize * sizeof (*state));
+  *returnColumnSizes = (int *) malloc (res_cnt * sizeof (int));
+  (*returnColumnSizes)[res_cnt - 1] = 0;
+  
+  subsets_inner (nums, numsSize, state, 0, 0, res, &res_num, *returnColumnSizes);
+  return res;
+}
+*/
+
 void main ()
 {
   int arr[] = {1, 2, 3}; 
