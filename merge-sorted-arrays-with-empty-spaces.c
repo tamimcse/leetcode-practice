90316894d19a3d946952b6a506b9086c2accdb5a
tamimcse <tamim@csebuet.org>
2021-06-01 20:26:18 -0400

    .

--- merge-sorted-arrays-with-empty-spaces.c
+++ merge-sorted-arrays-with-empty-spaces.c
@@ -26,6 +26,7 @@ void merge (int *arr1, int *arr2, int arr1_len, int arr2_len)
   }
 }
 
+//check beneath main
 void main ()
 {
   int arr1[] = {-1, 2, -1, 5, -1, 6, 9, -1, -1, 11};
@@ -41,3 +42,32 @@ void main ()
   }
   printf ("\n");
 }
+
+/*
+https://leetcode.com/problems/merge-sorted-array/
+
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
+  int i = m - 1, j = n -1, k = m + n - 1, p;
+  
+  if (m == 0) {
+    memcpy (nums1, nums2, n * sizeof (int));
+    return;
+  }
+  
+  while (j >= 0) {
+    if (nums2[j] >= nums1[i]) {
+      nums1[k--] = nums2[j--];
+    } else {
+      for (p = i; p >= 0; p--) {
+        if (nums1[p] > nums2[j]) {
+          nums1[p + 1] = nums1[p];
+        } else {
+          break;
+        }
+      }
+      nums1[p + 1] = nums2[j--];
+      i++;
+    }
+  }
+}
+*/
