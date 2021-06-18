e0cdfafb67afd97bb178603aac6159bcf4e2808f
tamimcse <tamim@csebuet.org>
2021-06-17 20:36:21 -0400

    .

---
+++ bitstream-mod5.c
@@ -0,0 +1,8 @@
+/*
+https://sites.google.com/site/yunrockqu/blog/interview-questions-1?authuser=0
+*/
+We have talked a lot about many design principles. If you think you are well-prepared, let me give you a real interview question in this article. I was asked to solve this question during my interview at Cisco, and I was able to answer it. You can use it as a good practice. 
+
+
+
+
