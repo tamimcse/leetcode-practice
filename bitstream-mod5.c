d961d610a60dc2238e2668c4295b206255af4a02
tamimcse <tamim@csebuet.org>
2021-06-18 14:59:00 -0400

    .

--- bitstream-mod5.c
+++ bitstream-mod5.c
@@ -1,11 +1,33 @@
 /*
 https://sites.google.com/site/yunrockqu/blog/interview-questions-1?authuser=0
 
-We have talked a lot about many design principles. If you think you are well-prepared, let me give you a real interview question in this article. I was asked to solve this question during my interview at Cisco, and I was able to answer it. You can use it as a good practice. 
+Cisco question.
+
+Given a single-bit streaming input, how do we report (mod 5) results of the stream? An example is shown in the following table: 
 
 Input:   1 0 1 1
 Output:  1 2 0 1 
 */
+#include <stdio.h>
+
+int num = 0;
+
+void insert (int a)
+{
+  num = (num << 1) | a;
+  printf ("%d ", num % 5);
+}
+
+void main ()
+{
+  int arr[] = {1, 0, 1, 1, 1};
+  int len = sizeof (arr) / sizeof (arr[0]);
+
+  for (int i = 0; i < len; i++) {
+    insert (arr[i]);
+  }
+  printf ("\n");
+}
 
 
 
