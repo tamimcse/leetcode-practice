8604ef04316a9a73d703d51a73329dcf07787382
tamimcse <tamim@csebuet.org>
2021-06-18 20:53:47 -0400

    .

---
+++ maximum-meetings-in-one-room.c
@@ -0,0 +1,9 @@
+/*
+https://www.geeksforgeeks.org/find-maximum-meetings-in-one-room/
+
+https://www.geeksforgeeks.org/activity-selection-problem-greedy-algo-1/
+
+Very interesting problem!!!
+
+Use greedy approach by sorting intervals on finished time.
+*/
