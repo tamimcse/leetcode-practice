6ad2797b6887155335205b32daed48426e6f28de
tamimcse <tamim@csebuet.org>
2021-06-27 21:25:18 -0400

    .

--- count-of-range-sum.c
+++ count-of-range-sum.c
@@ -1,25 +1,23 @@
 /*
 https://leetcode.com/problems/count-of-range-sum/
 
-Gets TLE
+Gets TLE. Solve this using Binary Index Tree
 */
 int countRangeSum(int* nums, int numsSize, int lower, int upper){
-  long *dp = (int *) calloc (numsSize * numsSize, sizeof (*dp));
+  long *prefix_sum = (long *) calloc (numsSize + 1, sizeof (*prefix_sum));
   int i, j, k, row, col, count, idx;
-
+  long range_sum;
+  
+  prefix_sum[0] = 0;
+  for (i = 0; i < numsSize; i++) {
+    prefix_sum[i+1] = prefix_sum[i] + nums[i];
+  }
+  
   count = 0;
   for (i = 0; i < numsSize; i++) {
-    idx = i * numsSize + i;
-    dp[idx] = nums[i];
-    if (dp[idx] >= lower && dp[idx] <= upper)
-      count++;
-  }
-
-  for (i = 1; i < numsSize; i++) {
-    for (row = 0, col = i; col < numsSize; row++, col++) {
-      idx = row * numsSize + col;
-      dp[idx] = dp[idx - 1] + nums[col];
-      if (dp[idx] >= lower && dp[idx] <= upper)
+    for (j = i; j < numsSize; j++) {
+      range_sum = prefix_sum[j+1] - prefix_sum[i];
+      if (range_sum >= lower && range_sum <= upper)
         count++;
     }
   }
