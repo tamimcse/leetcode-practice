a8e189974e58214508a4f01ee05da4664dbe601f
tamimcse <tamim@csebuet.org>
2021-06-30 19:53:42 -0400

    .

--- happy-number.c
+++ happy-number.c
@@ -1,3 +1,31 @@
 /*
+https://leetcode.com/problems/happy-number/
+
 https://www.geeksforgeeks.org/happy-number/
 */
+int next_num (int n)
+{
+  int sum = 0, digit;
+  while (n) {
+    digit = n % 10;
+    n = n / 10;
+    sum += (double)digit * digit;
+  }
+  return sum;
+}
+
+bool isHappy(int n){
+  if (n == 1)
+    return true;
+  
+  int slow = n, fast = n;
+  int next1, next2;
+  while (1) {
+    slow = next_num (slow);
+    fast = next_num (next_num (fast));
+    if (slow == 1)
+      return true;
+    if (slow == fast)
+      return false;
+  }
+}
