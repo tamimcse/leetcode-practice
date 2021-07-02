3de4fe938f11162244bd980aa206f255e6fb9474
tamimcse <tamim@csebuet.org>
2021-07-02 17:35:08 -0400

    .

--- linked-list-cycle.c
+++ linked-list-cycle.c
@@ -1,7 +1,7 @@
 /*
 https://leetcode.com/problems/linked-list-cycle/
 */
-//Use hashtable instead of stack
+//Use hashtable instead of stack. Also note that it is not very easy to hash a pointer. So, use slow and fast pointer approach instead
 bool hasCycle(struct ListNode *head) {
   struct ListNode *stack[10000] = {0};
   int idx = 0, i;
