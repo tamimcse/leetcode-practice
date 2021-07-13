3ee00b39d19607f340bc0e533da39aa490daf092
tamimcse <tamim@csebuet.org>
2021-07-13 19:34:22 -0400

    ..

---
+++ wiggle-sort-ii.c
@@ -0,0 +1,39 @@
+/*
+https://leetcode.com/problems/wiggle-sort-ii/
+
+Tried to solve in place. Look simple, but toooo tough!!
+
+
+*/
+void swap (int *arr, int i, int j)
+{
+  int tmp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = tmp;
+}
+
+int cmp (const void *a, const void *b)
+{
+  return *((int *)a) - *((int *)b);
+}
+
+void wiggleSort(int* nums, int numsSize){
+  int num_large_elements = numsSize >> 1;
+  int i, j, k;
+  int *tmp = (int *) malloc (numsSize * sizeof (*tmp));
+  
+  if (numsSize <= 1) {
+    return;
+  }
+  memcpy (tmp, nums, numsSize * sizeof (*tmp));
+  qsort (tmp, numsSize, sizeof (int), cmp);
+  
+  for (i = numsSize - num_large_elements - 1, j = numsSize - 1, k = 0;
+       k < numsSize; k++) {
+    if (k & 1) {
+      nums[k] = tmp[j--];
+    } else {
+      nums[k] = tmp[i--];
+    }
+  }
+}
