37593b3384a394ad5be4c14298fbbd5a54ab7b9c
tamimcse <tamim@csebuet.org>
2021-07-19 16:22:43 -0400

    .

---
+++ trim-a-binary-search-tree.c
@@ -0,0 +1,22 @@
+/*
+https://leetcode.com/problems/trim-a-binary-search-tree/
+*/
+struct TreeNode* trimBST(struct TreeNode* root, int low, int high){
+  if (!root)
+    return NULL;
+  if (root->val >= low && root->val <= high) {
+    root->left = trimBST (root->left, low, high);
+    root->right = trimBST (root->right, low, high);
+    return root;
+  }
+  
+  if (root->val < low) {
+    return trimBST (root->right, low, high);
+  }
+  
+  if (root->val > high) {
+    return trimBST (root->left, low, high);
+  }
+  
+  return NULL;
+}
