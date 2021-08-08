ceb2b251907789a520c8181debadcebb522c4dbe
tamimcse <tamim@csebuet.org>
2021-08-07 20:28:13 -0400

    .

---
+++ maximum-product-subarray.c
@@ -0,0 +1,22 @@
+/*
+https://leetcode.com/problems/maximum-product-subarray/
+*/
+int maxProduct(int* nums, int numsSize){
+  int i, j, prefix_prod, max = -11000000000000000;
+  
+  for (i = 0; i < numsSize; i++) {
+    if (nums[i] > max)
+      max = nums[i];
+    if (i + 1 >=  numsSize)
+      continue;
+    prefix_prod = nums[i] * nums[i + 1];
+    if (prefix_prod > max)
+      max = prefix_prod;
+    for (j = i + 2; j < numsSize; j++) {
+      prefix_prod *= nums[j];
+      if (prefix_prod > max)
+        max = prefix_prod;
+    }
+  }
+  return max;
+}
