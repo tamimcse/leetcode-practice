a5768b519bf6b42125e7a4886f726750d29a320c
tamimcse <tamim@csebuet.org>
2021-08-09 17:58:58 -0400

    .

--- maximum-product-subarray.c
+++ maximum-product-subarray.c
@@ -1,8 +1,16 @@
 /*
 https://leetcode.com/problems/maximum-product-subarray/
 
-Interetingly O(n^2) passes the test cases.
+Interetingly O(n^2) passes the test cases. But that is not enough!
+
+Follow the second solution of :
+
+https://www.geeksforgeeks.org/maximum-product-subarray/
+
 */
+
+/*
+//O(n^2) solution
 int maxProduct(int* nums, int numsSize){
   int i, j, prefix_prod, max = -11000000000000000;
   
@@ -21,4 +29,40 @@ int maxProduct(int* nums, int numsSize){
     }
   }
   return max;
+}
+
+*/
+
+
+int max_ (int a, int b)
+{
+  return a > b ? a : b;
+}
+
+int min_ (int a, int b)
+{
+  return a < b ? a : b;
+}
+
+int maxProduct(int* nums, int numsSize){
+  int *max = (int *) malloc (numsSize * sizeof (int));
+  int *min = (int *) malloc (numsSize * sizeof (int));
+  int i, cur_min, cur_max, res;
+  
+  max[0] = nums[0];
+  min[0] = nums[0];
+  
+  for (i = 1; i < numsSize; i++) {
+    cur_min = min_ (nums[i] * max[i-1], nums[i] * min[i-1]);
+    cur_max = max_ (nums[i] * max[i-1], nums[i] * min[i-1]);
+    max[i] = max_ (cur_max, nums[i]);
+    min[i] = min_ (cur_min, nums[i]);
+  }
+  
+  res = max[0];
+  for (i = 1; i < numsSize; i++) {
+    if (max[i] > res)
+      res = max[i];
+  }
+  return res;
 }
