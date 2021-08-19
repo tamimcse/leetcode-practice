020c40f2b7c30535a1431914459e4956424d6b2a
tamimcse <tamim@csebuet.org>
2021-08-19 16:39:32 -0400

    .

---
+++ longest-common-substring-array-strings.c
@@ -0,0 +1,9 @@
+/*
+https://www.geeksforgeeks.org/longest-common-substring-array-strings/
+
+I was asked this question at Juniper. I basicly tried to implement sliding window algorithm based on the first string. I think, it's more efficient.
+
+This solutio  however generates all possible substring of a strig and check if sub string exist in rest of the string. I think, this is not a good solution.
+*/
+
+
