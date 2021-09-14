2f5f141e3e0318a5d67c84aa41ffa87c9fa0f4ac
tamimcse <tamim@csebuet.org>
2021-09-14 18:52:48 -0400

    .

--- word-break.c
+++ word-break.c
@@ -10,6 +10,70 @@ very interesting problem!
 
 struct trie_node {
   bool leaf;
+  struct trie_node *children[26];  
+};
+
+struct trie_node* create_node ()
+{
+  struct trie_node *n = (struct trie_node*) calloc (1, sizeof (struct trie_node));
+  n->leaf = false;
+  return n;
+}
+
+void trie_insert (struct trie_node *root, char *s)
+{
+  struct trie_node *runner = root;
+  int i;
+  
+  //insert the string backwords  
+  for (i = strlen (s) - 1; i >= 0; i--) {
+    if (!runner->children[s[i] - 'a']) {
+      runner->children[s[i] - 'a'] =  create_node ();
+    }
+    runner = runner->children[s[i] - 'a'];  
+  }
+  runner->leaf = true;  
+}
+
+
+
+bool wordBreak(char * s, char ** wordDict, int wordDictSize){
+  struct trie_node root;
+  int i, j, len = strlen (s);
+  int *dp = (int *) calloc (len, sizeof (int));  
+  struct trie_node *runner;
+  bool found;  
+    
+  memset (&root, NULL, sizeof (struct trie_node));
+    
+  for (i = 0; i < wordDictSize; i++) {
+    trie_insert (&root, wordDict[i]);  
+  }
+    
+  for (i = 0; i < len; i++) {
+    runner = &root;
+    found = false;  
+    for (j = i; j >= 0; j--) {
+      if (!runner->children[s[j]-'a'])
+         break;
+      runner = runner->children[s[j]-'a'];
+      if (runner->leaf && (!j || dp[j - 1])) {
+         found = true;
+         break; 
+      } 
+    }
+    if (found)
+        dp[i] = 1;
+  }
+  
+  return dp[len-1] == 1;  
+}
+
+/*
+
+//leetcode first attempt
+struct trie_node {
+  bool leaf;
   struct trie_node *children[26];
 };
 
@@ -66,3 +130,5 @@ bool wordBreak(char * s, char ** wordDict, int wordDictSize){
   
   return trie_search (root, s);
 }
+
+*/
