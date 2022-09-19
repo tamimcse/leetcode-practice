bf0d15448ed2942f101634af163194d8a0efc532
tamimcse <tamim@csebuet.org>
2022-09-18 19:32:50 -0700

    .

---
+++ C++/41-kmp-algorithm-for-pattern-searching.cpp
@@ -0,0 +1,6 @@
+/*
+https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
+
+  //calculate the longest prefix length which is also a suffix of the subpattern pattern. 
+  //https://www.educative.io/edpresso/what-is-the-knuth-morris-pratt-algorithm
+*/
