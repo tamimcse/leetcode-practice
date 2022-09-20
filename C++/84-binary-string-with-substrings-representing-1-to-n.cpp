241b3a739dccc5ecf90d3ce31748cd78fa1cdba7
tamimcse <tamim@csebuet.org>
2022-09-19 19:59:12 -0700

    .

---
+++ C++/84-binary-string-with-substrings-representing-1-to-n.cpp
@@ -0,0 +1,7 @@
+/*
+https://leetcode.com/problems/binary-string-with-substrings-representing-1-to-n/
+
+Actual problem was:
+
+https://www.geeksforgeeks.org/interesting-method-generate-binary-numbers-1-n/
+*/
