b30f8b3b7097be614e375827c8fd448198eecd24
tamimcse <tamim@csebuet.org>
2022-09-20 21:01:35 -0700

    .

---
+++ C++/129-sort-array-by-parity.cpp
@@ -0,0 +1,5 @@
+/*
+https://leetcode.com/problems/sort-array-by-parity/
+
+I'm inclined to implement this using two pointer approach. But, it is better to implement using quick sort partitioning.
+*/
