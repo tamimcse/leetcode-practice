79141bb45f63dcf921617b018f551604eeed817b
tamimcse <tamim@csebuet.org>
2022-09-24 14:53:44 -0700

    .

--- C++/1-reverse-string.cpp
+++ C++/1-reverse-string.cpp
@@ -1,3 +1,18 @@
 /*
 https://leetcode.com/problems/reverse-string/
 */
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int start = 0, end = s.size() - 1;
+        int tmp;
+        
+        while (end > start) {
+            tmp = s[start];
+            s[start] = s[end];
+            s[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+};
