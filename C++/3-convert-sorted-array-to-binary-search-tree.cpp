490853a15855df25cedaf27121918c150f13e60f
tamimcse <tamim@csebuet.org>
2022-09-24 20:21:16 -0700

    .

--- C++/3-convert-sorted-array-to-binary-search-tree.cpp
+++ C++/3-convert-sorted-array-to-binary-search-tree.cpp
@@ -1,3 +1,18 @@
 /*
 https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 */
+class Solution {
+    TreeNode* sortedArrayToBST_inner(vector<int>& nums, int start, int end) {
+        if(start > end)
+            return nullptr;
+        int mid = (start + end) >> 1;
+        TreeNode *root = new TreeNode(nums[mid]);
+        root->left = sortedArrayToBST_inner (nums, start, mid - 1);
+        root->right = sortedArrayToBST_inner (nums, mid + 1, end);
+        return root;
+    }    
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return sortedArrayToBST_inner(nums, 0, nums.size() - 1);
+    }
+};
