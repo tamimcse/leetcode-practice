8b9142e9f9a4abd7efbc0ecb3102081f76661058
tamimcse <tamim@csebuet.org>
2022-09-25 15:52:15 -0700

    .

--- C++/8-backspace-string-compare.cpp
+++ C++/8-backspace-string-compare.cpp
@@ -1,3 +1,25 @@
 /*
 https://leetcode.com/problems/backspace-string-compare/
 */
+class Solution {
+    void final_string(string &s) {
+        int to_insert = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '#') {
+                if (to_insert > 0)
+                    to_insert--;
+            } else {
+                s[to_insert++] = s[i];
+            }
+        }
+        s.replace(to_insert, s.length() - to_insert, "");        
+    }
+public:
+    bool backspaceCompare(string s, string t) {
+        final_string(s);
+        cout << s << endl;
+        final_string(t);
+        cout << t << endl;
+        return s == t;
+    }
+};
