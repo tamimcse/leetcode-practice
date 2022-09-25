4f442c51218c1f648f97ba4c63229edbed82eadb
tamimcse <tamim@csebuet.org>
2022-09-24 21:01:43 -0700

    .

--- C++/5-reverse-bits.cpp
+++ C++/5-reverse-bits.cpp
@@ -1,3 +1,25 @@
 /*
 https://leetcode.com/problems/reverse-bits/
 */
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t mask1 = 1U, mask2 = 1U << 31;
+        while (mask2 > mask1) {
+            if (n & mask1) {
+                if ((n & mask2) == 0) {
+                    n |= mask2;
+                    n &= ~mask1;
+                }
+            } else {
+                if (n & mask2) {
+                    n |= mask1;
+                    n &= ~mask2;
+                }
+            }
+            mask1 <<= 1;
+            mask2 >>= 1;
+        }
+        return n;
+    }
+};
