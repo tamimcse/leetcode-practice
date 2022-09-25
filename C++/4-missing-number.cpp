1371e330032a3b515093e3f3432d8272a8ea1952
tamimcse <tamim@csebuet.org>
2022-09-24 20:25:51 -0700

    .

--- C++/4-missing-number.cpp
+++ C++/4-missing-number.cpp
@@ -1,3 +1,13 @@
 /*
 https://leetcode.com/problems/missing-number/
 */
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = 0, len = nums.size();
+        for (auto e : nums) {
+            sum += e;
+        }
+        return ((len * (len + 1)) >> 1) - sum;
+    }
+};
