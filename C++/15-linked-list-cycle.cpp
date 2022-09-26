1ab99de1f92a61084435e261b8ae9fc64df589a3
tamimcse <tamim@csebuet.org>
2022-09-25 20:33:29 -0700

    .

--- C++/15-linked-list-cycle.cpp
+++ C++/15-linked-list-cycle.cpp
@@ -1,3 +1,22 @@
 /*
 https://leetcode.com/problems/linked-list-cycle/
 */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *r1 = head, *r2 = head;
+        if (head == nullptr)
+            return false;
+        do {
+            r1 = r1->next;
+            r2 = r2->next;
+            if (r2)
+                r2 = r2->next;
+            else
+                return false;
+            if (r1 == r2)
+                return true;
+        } while (r1 && r2);
+        return false;
+    }
+};
