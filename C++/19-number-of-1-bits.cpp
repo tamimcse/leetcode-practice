6d8fb9c050dfb15498ff733a99d8da667ff95943
tamimcse <tamim@csebuet.org>
2022-09-25 21:58:56 -0700

    .

--- C++/19-number-of-1-bits.cpp
+++ C++/19-number-of-1-bits.cpp
@@ -1,3 +1,15 @@
 /*
 https://leetcode.com/problems/number-of-1-bits/
 */
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        
+        while (n) {
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+    }
+};
