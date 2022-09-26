cb3f73d15aae10283a2a6aa82b8dd1c4583eb0e8
tamimcse <tamim@csebuet.org>
2022-09-25 18:20:05 -0700

    .

--- C++/10-fibonacci-number.cpp
+++ C++/10-fibonacci-number.cpp
@@ -4,18 +4,8 @@ https://leetcode.com/problems/fibonacci-number/
 class Solution {
 public:
     int fib(int n) {
-        deque<int> lastTwo(2); 
-        lastTwo[0] = 0; 
-        lastTwo[1] = 1;
-        
-        if (n >= 0 && n <= 1)
+        if (n == 0 || n == 1)
             return n;
-        
-        for (int i = 2; i <= n; i++) {
-            lastTwo.push_back(lastTwo[0] + lastTwo[1]);
-            lastTwo.pop_front();
-        }
-        
-        return lastTwo[1];
+        return fib (n - 1) + fib(n - 2);
     }
 };
