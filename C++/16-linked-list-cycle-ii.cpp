3ec2e74094ffa76350e1d2c54550cdfeba9062e1
tamimcse <tamim@csebuet.org>
2022-09-25 21:19:51 -0700

    .

--- C++/16-linked-list-cycle-ii.cpp
+++ C++/16-linked-list-cycle-ii.cpp
@@ -1,3 +1,31 @@
 /*
 https://leetcode.com/problems/linked-list-cycle-ii/
 */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *r1 = head, *r2 = head;
+        if (head == nullptr)
+            return nullptr;
+        do {
+            r1 = r1->next;
+            r2 = r2->next;
+            if (r2)
+                r2 = r2->next;
+            else
+                return nullptr;
+            if (r1 == r2) {
+                r1 = head;
+                while (true) {
+                    if (r1 == r2) {
+                        return r1;
+                    } else {
+                        r1 = r1->next;
+                        r2 = r2->next;
+                    }
+                }
+            }
+        } while (r1 && r2);
+        return nullptr;
+    }
+};
