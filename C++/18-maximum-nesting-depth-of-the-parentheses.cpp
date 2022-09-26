6cb77f9b8ad70027954901f717c8281ea63a8fce
tamimcse <tamim@csebuet.org>
2022-09-25 21:55:59 -0700

    .

--- C++/18-maximum-nesting-depth-of-the-parentheses.cpp
+++ C++/18-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,3 +1,19 @@
 /*
 https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
 */
+class Solution {
+public:
+    int maxDepth(string s) {
+        int depth = 0, curr_depth = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                curr_depth++;
+                depth = max (depth, curr_depth);
+            } else if (s[i] == ')'){
+                curr_depth--;
+            }
+        }
+        return depth;
+    }
+};
