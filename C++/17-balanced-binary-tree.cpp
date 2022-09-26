3480a83d70ad013db0df84a86f26283676b663e0
tamimcse <tamim@csebuet.org>
2022-09-25 21:44:52 -0700

    .

--- C++/17-balanced-binary-tree.cpp
+++ C++/17-balanced-binary-tree.cpp
@@ -1,3 +1,23 @@
 /*
 https://leetcode.com/problems/balanced-binary-tree/
 */
+class Solution {
+    int height(TreeNode* root, bool &res) {
+        int left, right;
+        if (root == nullptr)
+            return 0;
+        left = root->left ? height(root->left, res) + 1 : 0;
+        right = root->right ? height(root->right, res) + 1 : 0;
+        if (abs (left - right) > 1)
+            res = false;
+        return max (left, right);
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        if (root == nullptr)
+            return true;
+        bool res = true;
+        height(root, res);
+        return res;
+    }
+};
