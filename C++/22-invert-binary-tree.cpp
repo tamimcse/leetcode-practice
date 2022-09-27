91c6d696ebc1e8801962580c4e1551204b2876b8
tamimcse <tamim@csebuet.org>
2022-09-26 21:45:04 -0700

    .

--- C++/22-invert-binary-tree.cpp
+++ C++/22-invert-binary-tree.cpp
@@ -1,3 +1,15 @@
 /*
 https://leetcode.com/problems/invert-binary-tree/
 */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr)
+            return nullptr;
+        TreeNode *left = invertTree (root->right);
+        TreeNode *right = invertTree (root->left);
+        root->left = left;
+        root->right = right;
+        return root;
+    }
+};
