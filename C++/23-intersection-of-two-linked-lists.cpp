db18cb213b51fa5e2311b73e7aa7b7ae292c22a3
tamimcse <tamim@csebuet.org>
2022-09-26 21:52:21 -0700

    .

--- C++/23-intersection-of-two-linked-lists.cpp
+++ C++/23-intersection-of-two-linked-lists.cpp
@@ -1,3 +1,23 @@
 /*
 https://leetcode.com/problems/intersection-of-two-linked-lists/
 */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_map<ListNode *, bool> node_list;
+        ListNode *r = headA;
+        while (r) {
+            node_list[r] = true;
+            r = r->next;
+        }
+        
+        r = headB;
+        while (r) {
+            auto it = node_list.find (r);
+            if (it != node_list.end())
+                return r;
+            r = r->next;
+        }
+        return nullptr;
+    }
+};
