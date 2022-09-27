c4f711eaeae483f06b84e301664ef4d634b8dcca
tamimcse <tamim@csebuet.org>
2022-09-26 20:56:18 -0700

    .

--- C++/20-symmetric-tree.cpp
+++ C++/20-symmetric-tree.cpp
@@ -22,3 +22,33 @@ public:
         return isSymmetric_inner (root->left, root->right);
     }
 };
+
+class Solution {
+
+public:
+    bool isSymmetric(TreeNode* root) {
+        TreeNode *tmp1, *tmp2;
+        if (root == nullptr)
+            return true;
+        queue<TreeNode*> q;
+        q.push(root->left);
+        q.push(root->right);
+        while (!q.empty()) {
+            tmp1 = q.front();
+            q.pop();
+            tmp2 = q.front();
+            q.pop();
+            if ((tmp1 && !tmp2) || (!tmp1 && tmp2))
+                return false;
+            if (!tmp1 && !tmp2)
+                continue;
+            if (tmp1->val != tmp2->val)
+                return false;
+            q.push(tmp1->left);
+            q.push(tmp2->right);
+            q.push(tmp1->right);
+            q.push(tmp2->left);
+        }
+        return true;
+    }
+};
