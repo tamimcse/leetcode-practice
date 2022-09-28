dea92709e629d10a75225b184436623c24c9862a
tamimcse <tamim@csebuet.org>
2022-09-27 18:16:00 -0700

    .

--- C++/24-maximum-depth-of-binary-tree.cpp
+++ C++/24-maximum-depth-of-binary-tree.cpp
@@ -1,3 +1,11 @@
 /*
 https://leetcode.com/problems/maximum-depth-of-binary-tree/
 */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        return max (maxDepth(root->left) + 1, maxDepth(root->right) + 1);
+    }
+};
