31afec613c88d787572e14a1a0b6465d63d8ceae
tamimcse <tamim@csebuet.org>
2022-09-27 19:43:33 -0700

    .

--- C++/26-middle-of-the-linked-list.cpp
+++ C++/26-middle-of-the-linked-list.cpp
@@ -1,3 +1,17 @@
 /*
 https://leetcode.com/problems/middle-of-the-linked-list/
 */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *r1 = head, *r2 = head;
+        
+        while (r2 != nullptr && r2->next) {
+            r1 = r1->next;
+            r2 = r2->next;
+            if (r2)
+                r2 = r2->next;
+        }
+        return r1;
+    }
+};
