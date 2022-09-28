36553db87c590441a8ee291e940d6e0d87d05a51
tamimcse <tamim@csebuet.org>
2022-09-27 21:21:44 -0700

    .

--- C++/30-subtree-of-another-tree.cpp
+++ C++/30-subtree-of-another-tree.cpp
@@ -1,3 +1,30 @@
 /*
 https://leetcode.com/problems/subtree-of-another-tree/
 */
+class Solution {
+    bool is_equal(TreeNode* root, TreeNode* subRoot) {
+        if (root == nullptr && subRoot == nullptr)
+            return true;
+        if (root == nullptr || subRoot == nullptr)
+            return false;
+        return root->val == subRoot->val && is_equal(root->left, subRoot->left) &&
+                                            is_equal(root->right, subRoot->right);
+    }
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        bool res;
+        if (root == nullptr || subRoot == nullptr)
+            return false;
+            
+        if (root->val == subRoot->val) {
+            res = is_equal (root, subRoot);
+            if (res)
+                return res;
+        }
+            
+        res = isSubtree (root->left, subRoot);
+        if (res)    
+            return res;
+        return isSubtree (root->right, subRoot);
+    }
+};
