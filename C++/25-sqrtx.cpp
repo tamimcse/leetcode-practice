4d5b0a6b235a3509d36cffb063ca6475658a6cb1
tamimcse <tamim@csebuet.org>
2022-09-27 19:38:25 -0700

    .

--- C++/25-sqrtx.cpp
+++ C++/25-sqrtx.cpp
@@ -1,3 +1,25 @@
 /*
 https://leetcode.com/problems/sqrtx/
 */
+class Solution {
+public:
+    int mySqrt(int x) {
+        int start = 0, end = x / 2, mid;
+        if (x < 2)
+            return x;
+        long long n1, n2;
+        while (end >= start) {
+            mid = (start + end) >> 1;
+            n1 = (long long)mid * mid;
+            n2 = (long long)(mid + 1) * (mid + 1);
+            if (n1 <= x && (x < n2)) {
+                return mid;
+            } else if (n1 < x) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return mid;
+    }
+};
