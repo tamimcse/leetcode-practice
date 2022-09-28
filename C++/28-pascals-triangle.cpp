09bbdd21e2cd98c60a0af68ecbcb4184b6febf2b
tamimcse <tamim@csebuet.org>
2022-09-27 21:04:31 -0700

    .

--- C++/28-pascals-triangle.cpp
+++ C++/28-pascals-triangle.cpp
@@ -1,3 +1,26 @@
 /*
 https://leetcode.com/problems/pascals-triangle/
 */
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res(numRows);
+        
+        for (int i = 0; i < numRows; i++) {
+            res[i].resize(i + 1);
+        }
+        
+        for (int i = 0; i < numRows; i++)
+            res[i][0] = 1;
+        
+        for (int i = 1, j = 1; i < numRows; i++, j++)
+            res[i][j] = 1;
+        
+        for (int i = 2; i < numRows; i++) {
+            for (int j = 1; j < i; j++) {
+                res[i][j] = res[i-1][j-1] + res[i-1][j];
+            }
+        }
+        return res;
+    }
+};
