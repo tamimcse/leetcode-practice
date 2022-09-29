5a66e6c9b516dfad5e077c68c5fe641d6336e56b
tamimcse <tamim@csebuet.org>
2022-09-28 19:39:55 -0700

    .

--- C++/36-path-sum.cpp
+++ C++/36-path-sum.cpp
@@ -1,3 +1,20 @@
 /*
 https://leetcode.com/problems/path-sum/
 */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == nullptr)
+            return false;
+        if (root->left == nullptr && root->right == nullptr)
+            return root->val == targetSum;
+        if (root->left) {
+            bool res = hasPathSum (root->left, targetSum - root->val);
+            if (res)
+                return true;
+        }
+        
+        return  hasPathSum (root->right, targetSum - root->val);
+            
+    }
+};
