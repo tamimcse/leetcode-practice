338c83625dd6b462bcefa09aec25a424bbaeee8e
tamimcse <tamim@csebuet.org>
2022-09-28 21:32:10 -0700

    .

--- C++/39-sort-array-by-parity.cpp
+++ C++/39-sort-array-by-parity.cpp
@@ -1,5 +1,26 @@
 /*
 https://leetcode.com/problems/sort-array-by-parity/
-
-I'm inclined to implement this using two pointer approach. But, it is better to implement using quick sort partitioning.
 */
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int odd_idx = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] & 1) {
+                odd_idx = i;
+                break;
+            }
+        }
+            
+        for (int i = 0; i < nums.size(); i++) {
+            if(((nums[i] & 1) == 0) && (i > odd_idx) && (odd_idx >= 0)) {
+                swap(nums[i], nums[odd_idx]);
+                odd_idx++;
+                while (odd_idx < nums.size() && (nums[odd_idx] % 2) == 0)
+                    odd_idx++;
+            }
+        }
+        
+        return nums;
+    }
+};
