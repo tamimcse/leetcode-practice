e3b28a7d99d2a70dec315baea6f6c7948de2b7ec
tamimcse <tamim@csebuet.org>
2022-09-28 18:46:45 -0700

    .

--- C++/34-climbing-stairs.cpp
+++ C++/34-climbing-stairs.cpp
@@ -1,3 +1,14 @@
 /*
 https://leetcode.com/problems/climbing-stairs/
 */
+class Solution {
+   int memory[46] = {0};
+public:
+    int climbStairs(int n) {
+        if (n == 0)
+            return 1;
+        if (!memory[n])
+            memory[n] = climbStairs(n - 1) + ((n >= 2) ? climbStairs (n - 2) : 0);;
+        return memory[n];
+    }
+};
