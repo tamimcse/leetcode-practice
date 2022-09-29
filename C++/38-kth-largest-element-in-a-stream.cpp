9dd76aba5bf1927d904daf3171bbdbd63f822eb5
tamimcse <tamim@csebuet.org>
2022-09-28 21:10:24 -0700

    .

--- C++/38-kth-largest-element-in-a-stream.cpp
+++ C++/38-kth-largest-element-in-a-stream.cpp
@@ -1,3 +1,31 @@
 /*
 https://leetcode.com/problems/kth-largest-element-in-a-stream/
 */
+class KthLargest {
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int th;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        for (auto e : nums) {
+            pq.push(e);
+        }
+
+        th = k;
+        
+        int num_delete = nums.size() - k;
+        for (int i = 0; i < num_delete; i++) {
+            if (!pq.empty())
+                pq.pop();
+        }
+    }
+    
+    int add(int val) {
+        if (pq.size() < th) {
+            pq.push(val);
+        } else if (val > pq.top()) {
+            pq.pop();
+            pq.push(val);   
+        }
+        return pq.top();
+    }
+};
