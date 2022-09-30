9c8ad498248933137409ad5f064fa02bae3d566b
tamimcse <tamim@csebuet.org>
2022-09-29 20:43:33 -0700

    .

--- C++/42-valid-anagram.cpp
+++ C++/42-valid-anagram.cpp
@@ -1,3 +1,23 @@
 /*
 https://leetcode.com/problems/valid-anagram/
 */
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int hash[26] = {0};
+        
+        for (auto &e : s) {
+            hash[e - 'a']++;
+        }
+        
+        for (auto &e : t) {
+            hash[e - 'a']--;
+        }
+        
+        for (int i = 0; i < 26; i++) {
+            if (hash[i] != 0)
+                return false;
+        }
+        return true;
+    }
+};
