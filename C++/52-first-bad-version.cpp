2f6a3c81e35bb0d25dabd90432ef181a4bc92b06
tamimcse <tamim@csebuet.org>
2022-09-29 21:50:25 -0700

    .

--- C++/52-first-bad-version.cpp
+++ C++/52-first-bad-version.cpp
@@ -1,3 +1,20 @@
 /*
 https://leetcode.com/problems/first-bad-version/
 */
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long start = 1, end = n, mid;
+        
+        while (end >= start) {
+            mid = ((long)start + end) >> 1;
+            if (!isBadVersion(mid))
+                start = mid + 1;
+            else if (mid == 0 || !isBadVersion(mid - 1))
+                return mid;
+            else
+                end = mid - 1;
+        }
+        return mid;
+    }
+};
