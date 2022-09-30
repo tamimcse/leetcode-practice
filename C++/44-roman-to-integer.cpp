ac6bbc6c44d38ea62fac61362ac45ca7cda0304f
tamimcse <tamim@csebuet.org>
2022-09-29 18:32:52 -0700

    .

--- C++/44-roman-to-integer.cpp
+++ C++/44-roman-to-integer.cpp
@@ -1,3 +1,32 @@
 /*
 https://leetcode.com/problems/roman-to-integer/
 */
+class Solution {
+    int value (char c) {
+        switch (c) {
+            case 'I':  return 1;
+            case 'V':  return 5;
+            case 'X':  return 10;
+            case 'L':  return 50;
+            case 'C':  return 100;
+            case 'D':  return 500;
+            case 'M':  return 1000;
+        }
+        return -1;
+    }
+public:
+    int romanToInt(string s) {
+        int sum = value (s[s.length() - 1]);
+        int prev = sum;
+        
+        for (int i = s.length() - 2; i >= 0; i--) {
+            int curr = value(s[i]);
+            if (curr >= prev)
+                sum += curr;
+            else 
+                sum -= curr;
+            prev = curr;
+        }
+        return sum;
+    }
+};
