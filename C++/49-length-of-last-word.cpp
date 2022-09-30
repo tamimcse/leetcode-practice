ce49159817217f5cd379ab96052d8f5e67d1b80f
tamimcse <tamim@csebuet.org>
2022-09-29 20:36:04 -0700

    .

--- C++/49-length-of-last-word.cpp
+++ C++/49-length-of-last-word.cpp
@@ -1,3 +1,21 @@
 /*
 https://leetcode.com/problems/length-of-last-word/
 */
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = s.length();
+        int i = len -1, end, start;
+        while (i >= 0 && s[i] == ' ')
+            i--;
+        if (i < 0)
+            return 0;
+        
+        end = i;
+        start = i;
+        while (start >= 0 && s[start] != ' ')
+            start--;
+        start++;
+        return end - start + 1;
+    }
+};
