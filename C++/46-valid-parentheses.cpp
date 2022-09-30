ee7fdb8707039a642de462b4bdef820585b9e595
tamimcse <tamim@csebuet.org>
2022-09-29 20:12:11 -0700

    .

--- C++/46-valid-parentheses.cpp
+++ C++/46-valid-parentheses.cpp
@@ -1,3 +1,25 @@
 /*
 https://leetcode.com/problems/valid-parentheses/
 */
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                stack.push(s[i]);
+            } else {
+                if (stack.empty())
+                    return false;
+                char top = stack.top();
+                stack.pop();
+                if ((s[i] == ')' && top != '(') || 
+                    (s[i] == '}' && top != '{') ||
+                    (s[i] == ']' && top != '['))
+                    return false;
+            }
+        }
+        return stack.empty();
+    }
+};
