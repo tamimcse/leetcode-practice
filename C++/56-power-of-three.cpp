7711ada4708bae83c1f8352c47e86e11c30cda76
tamimcse <tamim@csebuet.org>
2022-09-30 22:07:08 -0700

    .

--- C++/56-power-of-three.cpp
+++ C++/56-power-of-three.cpp
@@ -15,3 +15,15 @@ public:
         return n == 1;
     }
 };
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n <= 0)
+            return false;
+        
+       double x = log10(n) / log10(3);
+        cout << x << " " << ceil(x);
+        return ceil(x) == (int)x;
+    }
+};
