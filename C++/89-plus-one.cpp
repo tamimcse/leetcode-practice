d5f21ae9a2e77f6676663dd58487de789aca8958
tamimcse <tamim@csebuet.org>
2022-09-30 21:13:57 -0700

    .

--- C++/89-plus-one.cpp
+++ C++/89-plus-one.cpp
@@ -1,3 +1,20 @@
 /*
 https://leetcode.com/problems/plus-one/
 */
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry = 1;
+        
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            int num = digits[i] + carry;
+            digits[i] = num % 10;
+            carry = num / 10;
+        }
+        if (carry) {
+            digits.resize(digits.size() + 1);
+            digits[0] = 1;
+        }
+        return digits;
+    }
+};
