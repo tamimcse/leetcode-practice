df5a3819f481ec750cd6efc0b8b1811298816ca8
tamimcse <tamim@csebuet.org>
2022-09-30 21:57:00 -0700

    .

--- C++/57-power-of-three.cpp
+++ C++/57-power-of-three.cpp
@@ -1,3 +1,17 @@
 /*
 https://leetcode.com/problems/power-of-three/
+
+Do it both using loop and without loop.
 */
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (!n)
+            return false;
+        
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+};
