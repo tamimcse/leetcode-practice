16d23bc3191d94f8fe6f0fc519fd99f22d5719a1
tamimcse <tamim@csebuet.org>
2022-10-02 23:10:14 -0700

    .

--- C++/63-diameter-of-binary-tree.cpp
+++ C++/63-diameter-of-binary-tree.cpp
@@ -1,3 +1,27 @@
 /*
 https://leetcode.com/problems/diameter-of-binary-tree/
 */
+class Solution {
+    int diameter_inner (TreeNode* root, int &diameter) {
+        int left = 0, right = 0;
+        
+        if (root->left) {
+            left = diameter_inner (root->left, diameter) + 1;    
+        }
+        
+        if (root->right) {
+            right = diameter_inner (root->right, diameter) + 1;    
+        }
+        
+        diameter = max (left + right, diameter);
+        return max (left, right);
+    }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        int diameter = 0;
+        diameter_inner (root, diameter);
+        return diameter;
+    }
+};
