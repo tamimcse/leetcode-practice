52d9e797243510fa29d3edcd1e1e538b53765d39
tamimcse <tamim@csebuet.org>
2022-10-03 23:20:11 -0700

    .

--- C++/71-rotate-string.cpp
+++ C++/71-rotate-string.cpp
@@ -1,3 +1,19 @@
 /*
 https://leetcode.com/problems/rotate-string/
 */
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length())
+            return false;
+        if (s == goal)
+            return true;
+        for (int i = 1; i < s.length(); i++) {
+            string part1 = s.substr(0, i);
+            string part2 = s.substr(i, s.length() - i);
+            if (part2 + part1 == goal)
+                return true;
+        }
+        return false;
+    }
+};
