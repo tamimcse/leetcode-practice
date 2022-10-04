8bc404a850ceaa54e35b35e65375042a7b9038f9
tamimcse <tamim@csebuet.org>
2022-10-03 21:53:00 -0700

    .

--- C++/69-repeated-substring-pattern.cpp
+++ C++/69-repeated-substring-pattern.cpp
@@ -1,3 +1,27 @@
 /*
 https://leetcode.com/problems/repeated-substring-pattern/
 */
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        for (int i = 1; i < s.length(); i++) {
+            if (s[i] == s[0]) {
+                string pat = s.substr(0, i);
+                int start = i;
+                int end = i + i - 1;
+                //cout << pat << endl;
+                while (end < s.length()) {
+                    if (s.substr(start, i) == pat && end == s.length() - 1)
+                        return true;
+                    if (s.substr(start, i) == pat) {
+                        start = end + 1;
+                        end = start + i - 1;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
