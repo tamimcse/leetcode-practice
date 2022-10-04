8a1ff75913b62e0c856e015e9acc62b46d4c27c6
tamimcse <tamim@csebuet.org>
2022-10-03 18:22:53 -0700

    .

--- C++/66-logger-rate-limiter.cpp
+++ C++/66-logger-rate-limiter.cpp
@@ -1,7 +1,7 @@
 /*
 https://leetcode.com/problems/logger-rate-limiter/
 
-Here, Hash may contain many unnecessary entry. How to get rid of that?
+Here, Hash may contain many unnecessary entry. Reimplement it such that there is no unncessary entries in hash.
 */
 class Logger {
     unordered_map<string, int> hash;
@@ -21,6 +21,30 @@ public:
             }
         }
         hash[message] = timestamp;
+        return true;
+    }
+};
+
+class Logger {
+    unordered_map<string, int> hash;
+    queue<pair<string, int>> queue;
+public:
+    Logger() {
+        
+    }
+    
+    bool shouldPrintMessage(int timestamp, string message) {
+        while (!queue.empty() &&  (timestamp - queue.front().second >= 10)) {
+            hash.erase(queue.front().first);
+            queue.pop();
+        }
+        
+        auto it = hash.find(message);
+        if (it != hash.end()) {
+            return false;
+        }
+        hash[message] = timestamp;
+        queue.push(make_pair(message, timestamp));
         return true;
     }
 };
