8dfa26f03777b9c17ead70c6abeda4e4b31e89b9
tamimcse <tamim@csebuet.org>
2022-10-03 18:51:59 -0700

    .

--- C++/67-design-compressed-string-iterator.cpp
+++ C++/67-design-compressed-string-iterator.cpp
@@ -1,3 +1,39 @@
 /*
 https://leetcode.com/problems/design-compressed-string-iterator/
 */
+class StringIterator {
+    vector<pair<char, int>> vec;
+    int idx;
+public:
+    StringIterator(string compressedString) {
+        int i = 0, j;
+        char ch;
+        int count;
+        while (i < compressedString.length()) {
+            ch = compressedString[i];
+            j = i + 1;
+            while (j < compressedString.length() && isdigit(compressedString[j])) {
+                j++;
+            }
+            string str = compressedString.substr(i + 1, j - 1);
+            count = atoi(str.c_str());
+            vec.push_back (make_pair(ch, count));
+            i = j;       
+        }
+        idx = 0;
+    }
+    
+    char next() {
+        if (idx >= vec.size())
+            return ' ';
+        char ret = vec[idx].first;
+        vec[idx].second--;
+        if (!vec[idx].second)
+            idx++;
+        return ret;
+    }
+    
+    bool hasNext() {
+        return idx < vec.size();
+    }
+};
