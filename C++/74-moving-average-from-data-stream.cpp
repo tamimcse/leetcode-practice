5b148ee540e925b8b4de12af0f3e0449bd021998
tamimcse <tamim@csebuet.org>
2022-10-04 21:38:46 -0700

    .

--- C++/74-moving-average-from-data-stream.cpp
+++ C++/74-moving-average-from-data-stream.cpp
@@ -1,3 +1,29 @@
 /*
 https://leetcode.com/problems/moving-average-from-data-stream/
 */
+class MovingAverage {
+    queue<int> q;
+    int window_size;
+    int count;
+    int sum;
+public:
+    MovingAverage(int size) {
+        window_size = size;
+        count = 0;
+        sum = 0;
+    }
+    
+    double next(int val) {
+        if (count < window_size) {
+            q.push(val);
+            sum += val;
+            count++;           
+        } else {
+            sum -= q.front();
+            q.pop();
+            q.push(val);
+            sum += val;   
+        }
+        return (double)sum/count;
+    }
+};
