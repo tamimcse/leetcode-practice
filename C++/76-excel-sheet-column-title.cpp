794b7aabfe2fd5782f7daf2e712c4a5cf70bf5ad
tamimcse <tamim@csebuet.org>
2022-10-05 00:21:49 -0700

    ;

--- C++/76-excel-sheet-column-title.cpp
+++ C++/76-excel-sheet-column-title.cpp
@@ -1,3 +1,25 @@
 /*
 https://leetcode.com/problems/excel-sheet-column-title/
 */
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string res;
+        
+        while (columnNumber) {
+            if (columnNumber <= 26) {
+                res.insert(0, 1, 'A' + columnNumber - 1);
+                return res;
+            } else {
+                int rem = columnNumber % 26;
+                columnNumber /= 26;
+                if (rem == 0) {
+                    rem = 26;
+                    columnNumber--;
+                }
+                res.insert(0, 1, 'A' + rem - 1);
+            }
+        }
+        return res;
+    }
+};
