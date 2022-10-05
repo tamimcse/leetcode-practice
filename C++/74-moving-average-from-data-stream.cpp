b184a867da3667fe3ebbf829046f281d301874ad
tamimcse <tamim@csebuet.org>
2022-10-04 21:42:38 -0700

    .

--- C++/74-moving-average-from-data-stream.cpp
+++ C++/74-moving-average-from-data-stream.cpp
@@ -27,3 +27,31 @@ public:
         return (double)sum/count;
     }
 };
+
+//Implemented this using Deque. Didn't see performance benefit
+class MovingAverage {
+    deque<int> q;
+    int window_size;
+    int count;
+    int sum;
+public:
+    MovingAverage(int size) {
+        window_size = size;
+        count = 0;
+        sum = 0;
+    }
+    
+    double next(int val) {
+        if (count < window_size) {
+            q.push_front(val);
+            sum += val;
+            count++;           
+        } else {
+            sum -= q.back();
+            q.pop_back();
+            q.push_front(val);
+            sum += val;   
+        }
+        return (double)sum/count;
+    }
+};
