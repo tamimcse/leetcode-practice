eafd36b90f402b91caa4e2d2793c288b48916621
tamimcse <tamim@csebuet.org>
2022-10-04 21:52:19 -0700

    .

--- C++/75-flip-game.cpp
+++ C++/75-flip-game.cpp
@@ -1,3 +1,19 @@
 /*
 https://leetcode.com/problems/flip-game/
 */
+class Solution {
+public:
+    vector<string> generatePossibleNextMoves(string currentState) {
+        vector<string> res;
+        
+        for (int i = 1; i < currentState.length(); i++) {
+            if (currentState[i] == '+' && currentState[i-1] == '+') {
+                string s = currentState;
+                s[i] = '-';
+                s[i-1] = '-';
+                res.push_back(s);
+            }
+        }
+        return res;
+    }
+};
