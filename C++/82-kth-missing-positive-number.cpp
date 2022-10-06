1e03116c6c5cb43e4c2e4719e7e8984df054a8ca
tamimcse <tamim@csebuet.org>
2022-10-05 21:12:35 -0700

    .

--- C++/82-kth-missing-positive-number.cpp
+++ C++/82-kth-missing-positive-number.cpp
@@ -1,3 +1,25 @@
 /*
 https://leetcode.com/problems/kth-missing-positive-number/
 */
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int i = 0;
+        int cur = 1;
+        
+        while (k > 0) {
+            if (i < arr.size() && cur < arr[i]) {
+                cur++;
+                k--;
+            } else if (i < arr.size() && cur == arr[i]){
+                cur++;
+                i++;
+            } else {
+                cur++;
+                k--;
+            }
+        }
+        
+        return cur - 1;
+    }
+};
