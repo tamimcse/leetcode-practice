108f0e09edda3030f07b51a37e88908804d8dcca
tamimcse <tamim@csebuet.org>
2022-10-05 20:15:19 -0700

    .

--- C++/90-reverse-vowels-of-a-string.cpp
+++ C++/90-reverse-vowels-of-a-string.cpp
@@ -1,3 +1,27 @@
 /*
 https://leetcode.com/problems/reverse-vowels-of-a-string/
 */
+class Solution {
+    bool is_vowel (char c) {
+        c = tolower(c);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'; 
+    }
+public:
+    string reverseVowels(string s) {
+        int start = 0, end = s.length() - 1;
+        while (end > start) {
+            while (start < s.length() && !is_vowel(s[start])) {
+                start++;
+            }
+            while (end >= 0 && !is_vowel(s[end])) {
+                end--;
+            }
+            if (end > start) {
+                swap(s[start], s[end]);
+                start++;
+                end--;
+            }
+        }
+        return s;
+    }
+};
