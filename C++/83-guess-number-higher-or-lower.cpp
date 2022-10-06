9a0547e13b7b989080b273235de3cf8755a47659
tamimcse <tamim@csebuet.org>
2022-10-05 21:22:31 -0700

    .

--- C++/83-guess-number-higher-or-lower.cpp
+++ C++/83-guess-number-higher-or-lower.cpp
@@ -1,3 +1,21 @@
 /*
 https://leetcode.com/problems/guess-number-higher-or-lower/
 */
+class Solution {
+public:
+    int guessNumber(int n) {
+        long start = 1, end = n, mid;
+        
+        while (end >= start) {
+            mid = (start + end) >> 1;
+            int res = guess (mid);
+            if (!res)
+                return mid;
+            if (res == -1)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return -1;
+    }
+};
