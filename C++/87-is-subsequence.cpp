9e79472bc96a0adf71290c6aa5c77ad393231e5f
tamimcse <tamim@csebuet.org>
2022-10-07 19:43:38 -0700

    .

--- C++/87-is-subsequence.cpp
+++ C++/87-is-subsequence.cpp
@@ -1,3 +1,21 @@
 /*
 https://leetcode.com/problems/is-subsequence/
 */
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int to_find = 0;
+        
+        if (s == "")
+            return true;
+        
+        for (int i = 0; i < t.length(); i++) {
+            if (t[i] == s[to_find]) {
+                to_find++;
+                if (to_find == s.length())
+                    return true;
+            }
+        }
+        return false;
+    }
+};
