194ee3dd59c0dfab60805bb61ea254cc4d42cb3d
tamimcse <tamim@csebuet.org>
2022-10-08 15:06:27 -0700

    .

--- C++/90-binary-tree-postorder-traversal.cpp
+++ C++/90-binary-tree-postorder-traversal.cpp
@@ -1,5 +1,22 @@
 /*
 https://leetcode.com/problems/binary-tree-postorder-traversal/
 
-Do it without recursion
+Do it both with and without recursion. Do it with recursion just to practice how to implement tree travarsal using recursion.
 */
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        vector<int> left, right;
+        
+        if (!root)
+            return res;
+        left = postorderTraversal(root->left);
+        right = postorderTraversal(root->right);
+        res.insert(res.end(), left.begin(), left.end());
+        res.insert(res.end(), right.begin(), right.end());
+        res.push_back(root->val);
+        return res;
+    }
+};
