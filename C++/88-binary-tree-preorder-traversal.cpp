46b75e38fbfead43c926c1bf1b4726bf04362c92
tamimcse <tamim@csebuet.org>
2022-10-08 14:21:54 -0700

    .

--- C++/88-binary-tree-preorder-traversal.cpp
+++ C++/88-binary-tree-preorder-traversal.cpp
@@ -3,3 +3,25 @@ https://leetcode.com/problems/binary-tree-preorder-traversal/
 
 Do it without recursion
 */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> s;
+        TreeNode *cur;
+        
+        if (!root)
+            return res;
+        s.push(root);
+        while (!s.empty()) {
+            cur = s.top();
+            s.pop();
+            res.push_back(cur->val);
+            if (cur->right)
+                s.push(cur->right);
+            if (cur->left)
+                s.push(cur->left);
+        }
+        return res;
+    }
+};
