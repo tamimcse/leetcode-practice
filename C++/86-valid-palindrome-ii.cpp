a43ec3d05889e76c901552a333822d378159b1f0
tamimcse <tamim@csebuet.org>
2022-10-07 19:33:02 -0700

    .

--- C++/86-valid-palindrome-ii.cpp
+++ C++/86-valid-palindrome-ii.cpp
@@ -1,3 +1,33 @@
 /*
 https://leetcode.com/problems/valid-palindrome-ii/
 */
+class Solution {
+    bool isalindrome(string s) {
+        int start = 0, end = s.length() - 1;
+        
+        while (end > start) {
+            if (s[start] == s[end]) {
+                start++;
+                end--;
+            } else { 
+                return false;
+            }
+        }
+        return true;
+    }
+
+public:
+    bool validPalindrome(string s) {
+        int start = 0, end = s.length() - 1;
+        
+        while (end > start) {
+            if (s[start] == s[end]) {
+                start++;
+                end--;
+            } else { 
+                return isalindrome(s.substr(start, end - start)) || isalindrome(s.substr(start + 1, end - start));
+            }
+        }
+        return true;
+    }
+};
