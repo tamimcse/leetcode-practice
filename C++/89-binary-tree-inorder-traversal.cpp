e83a54990ca9849c337d1ffa0ceedff5b9bcdf37
tamimcse <tamim@csebuet.org>
2022-10-08 14:41:21 -0700

    .

--- C++/89-binary-tree-inorder-traversal.cpp
+++ C++/89-binary-tree-inorder-traversal.cpp
@@ -3,3 +3,31 @@ https://leetcode.com/problems/binary-tree-inorder-traversal/
 
 Do it without recursion.
 */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> s;
+        TreeNode *cur;
+        
+        if (!root)
+            return res;
+        
+        TreeNode *r = root;
+        while (r) {
+            s.push(r);
+            r = r->left;
+        }
+        while (!s.empty()) {
+            cur = s.top();
+            s.pop();
+            res.push_back(cur->val);
+            r = cur->right;
+            while (r) {
+                s.push(r);
+                r = r->left;
+            }
+        }
+        return res;
+    }
+};
