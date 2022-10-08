ab116ead4158590e484d31a7e483516a4ca0faa5
tamimcse <tamim@csebuet.org>
2022-10-08 14:16:36 -0700

    .

--- C++/87-remove-linked-list-elements.cpp
+++ C++/87-remove-linked-list-elements.cpp
@@ -1,3 +1,19 @@
 /*
 https://leetcode.com/problems/remove-linked-list-elements/
 */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        while (head != nullptr && head->val == val)
+            head = head->next;
+        
+        ListNode *r = head;
+        while (r && r->next) {
+            if (r->next->val == val)
+                r->next = r->next->next;
+            else
+                r = r->next;
+        }
+        return head;
+    }
+};
