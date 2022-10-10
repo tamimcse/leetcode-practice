ea551c6b803671d8fa7e8ab397711728a41e956e
tamimcse <tamim@csebuet.org>
2022-10-09 17:03:46 -0700

    .

--- C++/96-count-odd-numbers-in-an-interval-range.cpp
+++ C++/96-count-odd-numbers-in-an-interval-range.cpp
@@ -1,3 +1,16 @@
 /*
 https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
 */
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        if ((low & 1) == 0)
+            low++;
+        if ((high & 1) == 0)
+            high--;
+        if (high < low)
+            return 0;
+        else
+            return ((high - low) >> 1) + 1;
+    }
+};
