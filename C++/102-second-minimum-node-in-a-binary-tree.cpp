892cee13a3bc71ce3a791335a5377542c0fa0bd1
tamimcse <tamim@csebuet.org>
2022-10-09 21:51:33 -0700

    .

--- C++/102-second-minimum-node-in-a-binary-tree.cpp
+++ C++/102-second-minimum-node-in-a-binary-tree.cpp
@@ -1,3 +1,38 @@
 /*
 https://leetcode.com/problems/second-minimum-node-in-a-binary-tree/
 */
+class Solution {
+
+public:
+    int findSecondMinimumValue(TreeNode* root) {
+        int sec_min1, sec_min2;
+        
+        if (root->right == nullptr)
+            return -1;
+        if (root->left->val == root->right->val) {
+            sec_min1 = findSecondMinimumValue (root->left);
+            sec_min2 = findSecondMinimumValue (root->right);
+            if (sec_min1 == -1)
+                return sec_min2;
+            if (sec_min2 == -1)
+                return sec_min1;
+            return min(sec_min1, sec_min2);
+        } else {
+            if (root->left->val < root->right->val) {
+                sec_min1 = findSecondMinimumValue (root->left);
+                if (root->right->val == 3)
+                    cout << sec_min1;
+                if (sec_min1 != -1)
+                    return min (sec_min1, root->right->val);
+                else
+                    return root->right->val;
+            } else {
+                sec_min1 = findSecondMinimumValue (root->right);
+                if (sec_min1 != -1)
+                    return min (sec_min1, root->left->val);
+                else
+                    return root->left->val;
+            }
+        }
+    }
+};
--- C++/105-find-if-path-exists-in-graph.cpp
+++ C++/105-find-if-path-exists-in-graph.cpp
@@ -1,3 +1,34 @@
 /*
 https://leetcode.com/problems/find-if-path-exists-in-graph/
 */
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        vector<vector<int>> adj_list(n);
+        int *hash = new int[n];
+        queue<int> q;
+        int cur;
+        
+        for (int i = 0; i < edges.size(); i++) {
+            adj_list[edges[i][0]].push_back(edges[i][1]);
+            adj_list[edges[i][1]].push_back(edges[i][0]);
+        }
+        
+        for (int i = 0; i < n; i++)
+            hash[i] = 0;
+        
+        q.push(source);
+        while (!q.empty()) {
+            cur = q.front();
+            q.pop();
+            if (cur == destination)
+                return true;
+            hash[cur] = 1;
+            for (int i = 0; i < adj_list[cur].size(); i++) {
+                if (!hash[adj_list[cur][i]])
+                    q.push(adj_list[cur][i]);
+            }
+        }
+        return false;
+    }
+};
