fe8ffb5a92304de0353f1d134d159df6162cd41b
tamimcse <tamim@csebuet.org>
2022-10-09 20:21:42 -0700

    .

--- C++/100-maximum-average-subarray-i.cpp
+++ C++/100-maximum-average-subarray-i.cpp
@@ -1,3 +1,22 @@
 /*
 https://leetcode.com/problems/maximum-average-subarray-i/
 */
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int sum = 0, max;
+        
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+    
+        max = sum;
+        for (int i = k; i < nums.size(); i++) {
+            sum += nums[i] - nums[i - k];
+            if (sum > max)
+                max = sum;
+        }
+        
+        return (double)max/k;
+    }
+};
