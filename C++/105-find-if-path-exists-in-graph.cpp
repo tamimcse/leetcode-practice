c751f22f61eac253402bc4605bd8bf13cacdbc77
tamimcse <tamim@csebuet.org>
2022-10-09 21:53:47 -0700

    .

--- C++/105-find-if-path-exists-in-graph.cpp
+++ C++/105-find-if-path-exists-in-graph.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         vector<vector<int>> adj_list(n);
-        int *hash = new int[n];
+        int *hash = new int[n]();
         queue<int> q;
         int cur;
         
@@ -13,9 +13,6 @@ public:
             adj_list[edges[i][0]].push_back(edges[i][1]);
             adj_list[edges[i][1]].push_back(edges[i][0]);
         }
-        
-        for (int i = 0; i < n; i++)
-            hash[i] = 0;
         
         q.push(source);
         while (!q.empty()) {
