bf3e25e207a0fecc52d3b71fb68f8289b2f250e5
tamimcse <tamim@csebuet.org>
2022-10-09 20:15:44 -0700

    .

--- C++/99-valid-perfect-square.cpp
+++ C++/99-valid-perfect-square.cpp
@@ -1,3 +1,22 @@
 /*
 https://leetcode.com/problems/valid-perfect-square/
 */
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if (num == 1)
+            return true;
+        long start = 1, end = num >> 1, mid;
+        
+        while (end >= start) {
+            mid = (start + end) >> 1;
+            if (mid * mid == num)
+                return true;
+            if (mid * mid > num)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return false;
+    }
+};
