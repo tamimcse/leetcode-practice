5fcb3cef8bb9a320b899a3c12f07bf5544e76aa9
tamimcse <tamim@csebuet.org>
2022-10-11 00:08:51 -0700

    .

--- C++/103-valid-word-abbreviation.cpp
+++ C++/103-valid-word-abbreviation.cpp
@@ -1,3 +1,28 @@
 /*
 https://leetcode.com/problems/valid-word-abbreviation/
 */
+class Solution {
+public:
+    bool validWordAbbreviation(string word, string abbr) {
+        int i = 0, j = 0;
+        while (i < word.length() && j < abbr.length()) {
+            if (isdigit(abbr[j]) && abbr[j] != '0') {
+                int k = j;
+                while (k < abbr.length() && isdigit(abbr[k])) {
+                    k++;
+                }
+                int skip = stoi(abbr.substr(j, k -j));
+                if (i + skip - 1 >= word.length())
+                    return false;
+                i += skip;
+                j = k;
+            } else if (word[i] != abbr[j]) {
+                return false;
+            } else {
+                i++;
+                j++;
+            }
+        }
+        return i == word.length() && j == abbr.length();       
+    }
+};
