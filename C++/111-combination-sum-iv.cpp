5685b9cb971d1bd7031a4a906c1690e4eee37eb8
tamimcse <tamim@csebuet.org>
2022-10-13 19:57:45 -0700

    .

--- C++/111-combination-sum-iv.cpp
+++ C++/111-combination-sum-iv.cpp
@@ -1,3 +1,29 @@
 /*
 https://leetcode.com/problems/combination-sum-iv/
 */
+class Solution {
+    int combinationSum4_inner(vector<int>& nums, int target, unordered_map<int, int> &hash) {
+        int count = 0, ret;
+        
+        if (hash.find(target) != hash.end())
+            return hash[target];
+        
+        if (!target)
+            return 1;
+        
+        for (auto e : nums) {
+            if (e > target)
+                continue;
+            ret = combinationSum4_inner(nums, target - e, hash);
+            hash[target - e] = ret;
+            count += ret;
+        }
+        return count;
+    }
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+        
+        return combinationSum4_inner(nums, target, hash);
+    }
+};
