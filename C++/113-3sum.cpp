498a413692198f77827d710d2cb110106efc1474
tamimcse <tamim@csebuet.org>
2022-10-13 22:11:45 -0700

    .

--- C++/113-3sum.cpp
+++ C++/113-3sum.cpp
@@ -1,3 +1,35 @@
 /*
 https://leetcode.com/problems/3sum/
 */
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        int target_sum;
+        
+        sort (nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            target_sum = 0 - nums[i];
+            unordered_set<int> set;
+            for (int j = i + 1; j < nums.size();) {
+                if (set.count(target_sum - nums[j])) {
+                    vector<int> inner (3);
+                    inner[0] = nums[i];
+                    inner[1] = nums[j];
+                    inner[2] = target_sum - nums[j];
+                    res.push_back(inner);
+                    j++;
+                    while (j < nums.size() && nums[j] == nums[j-1])
+                        j++;
+                    continue;
+                }
+                set.insert(nums[j]);
+                j++;
+            }
+        }
+        return res;
+    }
+};
