732dd876610f9c3686a201748a1ed085f14fcc3c
tamimcse <tamim@csebuet.org>
2022-10-14 21:32:30 -0700

    .

--- C++/126-delete-node-in-a-linked-list.cpp
+++ C++/126-delete-node-in-a-linked-list.cpp
@@ -1,3 +1,15 @@
 /*
 https://leetcode.com/problems/delete-node-in-a-linked-list/
 */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        while (node->next) {
+            node->val = node->next->val;
+            if (node->next->next == nullptr)
+                node->next = nullptr;
+            else
+                node = node->next;
+        }
+    }
+};
