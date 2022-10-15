e381cbe1207610f4c6dc01fc42e155fc0b7e5448
tamimcse <tamim@csebuet.org>
2022-10-15 01:29:07 -0700

    .

--- C++/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ C++/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,3 +1,24 @@
 /*
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 */
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min = prices[0], max = prices[0], profit = 0;
+        
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] < max) {
+                profit += max - min;
+                min = prices[i];
+                max = prices[i];
+            } else if (prices[i] < min) {
+                min = prices[i];
+                max = prices[i];
+            } else if (prices[i] > max) {
+                max = prices[i];
+            }
+        }
+        profit += max - min;
+        return profit;
+    }
+};
