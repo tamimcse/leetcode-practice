92936b12f5a68c0d4f1311988687bd616dbb99ca
tamimcse <tamim@csebuet.org>
2022-10-15 19:40:04 -0700

    .

--- C++/124-validate-binary-search-tree.cpp
+++ C++/124-validate-binary-search-tree.cpp
@@ -1,5 +1,7 @@
 /*
 https://leetcode.com/problems/validate-binary-search-tree/
+
+Implement both ways (find both ways)
 */
 class Solution {
     bool isValidBST_inner(TreeNode* root, long min, long max) {
@@ -19,5 +21,31 @@ class Solution {
 public:
     bool isValidBST(TreeNode* root) {
         return isValidBST_inner(root, (long)INT_MIN * 2, (long)INT_MAX * 2);
+    }
+};
+
+class Solution {
+    
+    bool isValidBST_inner(TreeNode* root, TreeNode* &prev) {
+        bool ret;
+        
+        if (!root)
+            return true;
+        ret = isValidBST_inner(root->left, prev);
+        if (!ret)
+            return false;
+        if (prev && root->val <= prev->val)
+            return false;
+        prev = root;
+        ret = isValidBST_inner(root->right, prev);
+        if (!ret)
+            return false;
+        return true;
+    }
+
+public:
+    bool isValidBST(TreeNode* root) {
+        TreeNode* prev = nullptr;
+        return isValidBST_inner(root, prev);
     }
 };
