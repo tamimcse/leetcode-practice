cf67d5da833a63e031188bbc271d4a2c251187c1
tamimcse <tamim@csebuet.org>
2022-10-16 16:47:09 -0700

    .

--- C++/130-kth-largest-element-in-an-array.cpp
+++ C++/130-kth-largest-element-in-an-array.cpp
@@ -1,3 +1,22 @@
 /*
 https://leetcode.com/problems/kth-largest-element-in-an-array/
 */
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        
+        for (auto e : nums) {
+            if (pq.size() < k) {
+                pq.push(e);
+                continue;
+            }
+            if (pq.top() >= e)
+                continue;
+            pq.pop();
+            pq.push(e);
+        }
+        
+        return pq.top();
+    }
+};
