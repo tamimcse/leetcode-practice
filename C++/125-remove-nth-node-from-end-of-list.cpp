2b5661aca1ed8a5c10387896c72381d871f02f16
tamimcse <tamim@csebuet.org>
2022-10-15 19:57:27 -0700

    .

--- C++/125-remove-nth-node-from-end-of-list.cpp
+++ C++/125-remove-nth-node-from-end-of-list.cpp
@@ -29,3 +29,26 @@ public:
         return head;
     }
 };
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int count = 0;
+        ListNode *r1 = head, *r2 = head;
+        
+        for (int i = 0; i < n; i++) {
+            if (r2)
+                r2 = r2->next;
+        }
+        
+        if (!r2)
+            return head->next;
+        while (r2->next) {
+            r1 = r1->next;
+            r2 = r2->next;
+        }
+        
+        r1->next = r1->next->next;
+        return head;
+    }
+};
