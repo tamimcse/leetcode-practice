7e5bbefecb4978253fcd14b8c32358ef0b04e9e6
tamimcse <tamim@csebuet.org>
2022-10-18 21:55:35 -0700

    .

--- C++/135-unique-paths.cpp
+++ C++/135-unique-paths.cpp
@@ -1,3 +1,27 @@
 /*
 https://leetcode.com/problems/unique-paths/
 */
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m);
+        
+        for (int i = 0; i < m; i++) {
+            dp[i].resize(n);
+        }
+        
+        for (int i = 0; i < n; i++)
+            dp[m-1][i] = 1;
+        
+        for (int i = 0; i < m; i++)
+            dp[i][n-1] = 1;
+        
+        for (int x = m - 2; x >= 0; x--) {
+            for (int y = n - 2; y >= 0; y--) {
+                dp[x][y] = dp[x][y+1] + dp[x+1][y];
+            }
+        }
+        
+        return dp[0][0];
+    }
+};
