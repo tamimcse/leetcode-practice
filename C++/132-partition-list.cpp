5b238b8819d15cba1624a584df03affa9d838e18
tamimcse <tamim@csebuet.org>
2022-10-18 19:10:00 -0700

    .

--- C++/132-partition-list.cpp
+++ C++/132-partition-list.cpp
@@ -1,3 +1,36 @@
 /*
 https://leetcode.com/problems/partition-list/
 */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode *r = head, *first, *r1;
+        ListNode second;
+        ListNode *second_runner = &second;
+        while (head && head->val >= x) {
+            second_runner->next = head;
+            head = head->next;
+            second_runner = second_runner->next;
+            second_runner->next = NULL;
+        }
+        
+        if (!head) {
+            return second.next;
+        }
+        
+        first = head;
+        r = head;
+        while (r->next) {
+            if (r->next->val >= x) {
+                second_runner->next = r->next;
+                second_runner = second_runner->next;
+                r->next = r->next->next;
+                second_runner->next = NULL;
+            } else {
+                r = r->next;    
+            }
+        }
+        r->next = second.next;
+        return first;
+    }
+};
