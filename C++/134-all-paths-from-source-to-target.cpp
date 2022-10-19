76e0ef2caf2b9f448dae13bc805bd8ee7b624fd3
tamimcse <tamim@csebuet.org>
2022-10-18 21:45:28 -0700

    .

--- C++/134-all-paths-from-source-to-target.cpp
+++ C++/134-all-paths-from-source-to-target.cpp
@@ -1,3 +1,27 @@
 /*
 https://leetcode.com/problems/all-paths-from-source-to-target/
 */
+class Solution {
+    void allPathsSourceTarget_inner(vector<vector<int>>& graph, int start, vector<int> &cur_res, 
+                              vector<vector<int>> &res) {
+        cur_res.push_back(start);
+        
+        if (start == graph.size() - 1) {
+            res.push_back(cur_res);
+            return;
+        }
+        
+        for (auto e : graph[start]) {
+            allPathsSourceTarget_inner(graph, e, cur_res, res);
+            cur_res.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> res;
+        vector<int> cur_res;
+        
+        allPathsSourceTarget_inner(graph, 0, cur_res, res);
+        return res;
+    }
+};
