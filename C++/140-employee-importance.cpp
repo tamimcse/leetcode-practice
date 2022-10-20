3919edde27a418dace7b2e4cd8500574aa545cb8
tamimcse <tamim@csebuet.org>
2022-10-19 21:20:54 -0700

    .

--- C++/140-employee-importance.cpp
+++ C++/140-employee-importance.cpp
@@ -1,3 +1,25 @@
 /*
 https://leetcode.com/problems/employee-importance/
 */
+class Solution {
+    int getImportance_inner (unordered_map<int, Employee*> &hash, int id) {
+        Employee *e = hash[id];
+        int sum = e->importance;
+
+        for (auto e : e->subordinates) {
+            sum += getImportance_inner(hash, e);
+        }
+        return sum;
+    }
+public:
+    int getImportance(vector<Employee*> employees, int id) {
+        unordered_map<int, Employee*> hash;
+        
+        for (auto &e : employees) {
+            hash[e->id] = e;
+        }
+        
+        return getImportance_inner(hash, id);
+        
+    }
+};
