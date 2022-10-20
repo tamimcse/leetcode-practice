c675ee50f992b08228eba1f9e8891e3130351478
tamimcse <tamim@csebuet.org>
2022-10-19 21:46:07 -0700

    .

--- C++/150-powx-n.cpp
+++ C++/150-powx-n.cpp
@@ -1,3 +1,32 @@
 /*
 https://leetcode.com/problems/powx-n/
 */
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long N = n;
+        bool neg = n < 0;
+        double res;
+        
+        
+        if (neg)
+            N = -N;
+        
+        if (N == 0)
+            return 1;
+        if (N == 1)
+            res = x;
+        else if (N == 2)
+            res = x * x;
+        else {
+            res = myPow(x, N/2);
+            res = res * res;
+            if (N & 1)
+                res *= x;
+        }
+        
+        if (neg)
+            res = 1/res;
+        return res;
+    }
+};
