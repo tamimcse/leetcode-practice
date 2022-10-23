674064f890fdc6417cbad29dfa09415d8a8dbd80
tamimcse <tamim@csebuet.org>
2022-10-23 16:26:17 -0700

    .

--- C++/149-binary-tree-zigzag-level-order-traversal.cpp
+++ C++/149-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,3 +1,44 @@
 /*
 https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
 */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        int turn = 0;
+        stack<TreeNode*> s1, s2;
+        TreeNode *cur;
+        
+        if (!root)
+            return res;
+        
+        s1.push(root);
+        while (!s1.empty() || !s2.empty()) {
+            vector<int> tmp;
+            if (!turn) {
+                while (!s1.empty()) {
+                    cur = s1.top();
+                    s1.pop();
+                    tmp.push_back(cur->val);
+                    if (cur->left)
+                        s2.push(cur->left);
+                    if (cur->right)
+                        s2.push(cur->right);                    
+                }
+            } else {
+                while (!s2.empty()) {
+                    cur = s2.top();
+                    s2.pop();
+                    tmp.push_back(cur->val);
+                    if (cur->right)
+                        s1.push(cur->right);
+                    if (cur->left)
+                        s1.push(cur->left);                    
+                }
+            }
+            turn = turn ^ 1;
+            res.push_back(tmp);
+        }
+        return res;
+    }
+};
