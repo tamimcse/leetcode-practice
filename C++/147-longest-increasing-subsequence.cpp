5d6feacb85983586140dcb16a14a92a1f1f2bcd5
tamimcse <tamim@csebuet.org>
2022-10-23 15:42:37 -0700

    .

--- C++/147-longest-increasing-subsequence.cpp
+++ C++/147-longest-increasing-subsequence.cpp
@@ -1,3 +1,25 @@
 /*
 https://leetcode.com/problems/longest-increasing-subsequence/
 */
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> count(nums.size());
+        
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i])
+                    count[i] = max(count[i], count[j] + 1);
+            }
+            if (!count[i])
+                count[i] = 1;
+        }
+        
+        int max = 0;
+        for (int i = 0; i < count.size(); i++) {
+            if (count[i] > max)
+                max = count[i];
+        }
+        return max;
+    }
+};
