93011673f448c8e85bd50a9c47cc198156871396
tamimcse <tamim@csebuet.org>
2022-10-23 21:36:02 -0700

    .

--- C++/159-clone-binary-tree-with-random-pointer.cpp
+++ C++/159-clone-binary-tree-with-random-pointer.cpp
@@ -1,3 +1,39 @@
 /*
 https://leetcode.com/problems/clone-binary-tree-with-random-pointer/
 */
+class Solution {
+    NodeCopy* copyTree(Node* root, unordered_map<Node*, NodeCopy*> &hash) {
+        NodeCopy *n = new NodeCopy(root->val);
+        hash[root] = n;
+        
+        if (root->left)
+            n->left = copyTree(root->left, hash);
+        
+        if (root->right)
+            n->right = copyTree(root->right, hash);
+        
+        return n;
+    }
+    
+    void copyRandPtr(Node* root, unordered_map<Node*, NodeCopy*> &hash) {
+        if (root->random)
+            hash[root]->random = hash[root->random];
+        
+        if (root->left)
+            copyRandPtr(root->left, hash);
+        
+        if (root->right)
+            copyRandPtr(root->right, hash);
+    }
+public:
+    NodeCopy* copyRandomBinaryTree(Node* root) {
+        unordered_map<Node*, NodeCopy*> hash;
+        
+        if (!root)
+            return nullptr;
+        
+        NodeCopy *new_root = copyTree (root, hash);
+        copyRandPtr(root, hash);
+        return new_root;
+    }
+};
