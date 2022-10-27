97e4ff9cd42b2c6a3f1c993850e57668f2723cae
tamimcse <tamim@csebuet.org>
2022-10-27 16:18:48 -0700

    .

--- C++/295-h-index.cpp
+++ C++/295-h-index.cpp
@@ -1,3 +1,17 @@
 /*
 https://leetcode.com/problems/h-index/
 */
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        sort (citations.begin(), citations.end());
+        
+        int j = 1;
+        for (int i = n - 1; i >= 0; i--, j++) {
+            if (citations[i] < j)
+                break;
+        }
+        return j-1;
+    }
+};
