0867e25adddf3020f94506755b4749e83bb5cb6b
tamimcse <tamim@csebuet.org>
2022-10-26 19:57:51 -0700

    .

--- C++/236-peak-index-in-a-mountain-array.cpp
+++ C++/236-peak-index-in-a-mountain-array.cpp
@@ -1,3 +1,20 @@
 /*
 https://leetcode.com/problems/peak-index-in-a-mountain-array/
 */
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int start = 1, end = arr.size() - 2, mid;
+        
+        while (end >= start) {
+            mid = (start + end) >> 1;
+            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
+                return mid;
+            else if (arr[mid] > arr[mid - 1])
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return 0;
+    }
+};
