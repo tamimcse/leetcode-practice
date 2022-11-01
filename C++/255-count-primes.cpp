c6fe82e70f55ab37b56cb38a42b27b27d5badd94
tamimcse <tamim@csebuet.org>
2022-10-31 21:12:39 -0700

    .

--- C++/255-count-primes.cpp
+++ C++/255-count-primes.cpp
@@ -1,3 +1,25 @@
 /*
 https://leetcode.com/problems/count-primes/
 */
+class Solution {
+public:
+    int countPrimes(int n) {
+        int k = sqrt(n), count = 0;
+        int *arr[] = new int[n]();
+        
+        for (int i = 2; i <= k; i++) {
+            m = i + i;
+            while (m < n) {
+                arr[m] = 1;
+                m += i;
+            }
+        }
+        
+        for (int i = 2; i <= n - 1; i++) {
+            if (!arr[i])
+                count++;
+        }
+        
+        return count;
+    }
+};
