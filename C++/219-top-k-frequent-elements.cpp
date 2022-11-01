bb3cc5f7501ce849332b3071aae63eb3d239fd74
tamimcse <tamim@csebuet.org>
2022-10-31 20:28:45 -0700

    .

--- C++/219-top-k-frequent-elements.cpp
+++ C++/219-top-k-frequent-elements.cpp
@@ -1,3 +1,31 @@
 /*
 https://leetcode.com/problems/top-k-frequent-elements/
 */
+struct Cmp {
+    bool operator()(pair<int, int> &p1, pair<int, int> &p2) {
+        return p1.second < p2.second;
+    }    
+};
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> hash;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, Cmp> pq;
+        vector<int> res(k);
+        
+        for (auto e : nums)
+            hash[e]++;
+        
+        for (auto &e : hash) {
+            pq.push(e);
+        }
+        
+        for (int i = 0; i < k; i++) {
+            res[i] = pq.top().first;
+            pq.pop();
+        }
+        
+        return res;
+    }
+};
