4e548c157b8d29fdca4087b0d7ebd2379653801f
tamimcse <tamim@csebuet.org>
2022-11-02 01:57:10 -0700

    .

--- C++/278-design-hit-counter.cpp
+++ C++/278-design-hit-counter.cpp
@@ -1,3 +1,21 @@
 /*
 https://leetcode.com/problems/design-hit-counter/
 */
+class HitCounter {
+    queue<int> q;
+public:
+    HitCounter() {
+        
+    }
+    
+    void hit(int timestamp) {
+        q.push(timestamp);
+    }
+    
+    int getHits(int timestamp) {
+        while (!q.empty() && (timestamp - q.front() >= 300)) {
+            q.pop();
+        }
+        return q.size();
+    }
+};
