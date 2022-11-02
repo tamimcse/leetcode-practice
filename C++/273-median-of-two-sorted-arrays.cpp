b80e33bf5b166b6af1f2d295dc2c52373353040b
tamimcse <tamim@csebuet.org>
2022-11-01 21:34:54 -0700

    .

--- C++/273-median-of-two-sorted-arrays.cpp
+++ C++/273-median-of-two-sorted-arrays.cpp
@@ -1,48 +1,40 @@
 /*
 https://leetcode.com/problems/median-of-two-sorted-arrays/
 */
+struct CmpMax {
+    bool operator()(int a, int b) {
+        return a < b;
+    }    
+};
 class Solution {
-    const int magic_num = 1000001;
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size(), m = nums2.size();
-        bool odd;
-        int target, itr = 0;
-        auto r1 = nums1.begin(), r2 = nums2.begin();
-        int item1, item2;
+        priority_queue<int, vector<int>, CmpMax> pq;
+        int m = nums1.size(), n = nums2.size();
         
-        if ((n + m) & 1) {
-            target = ((n + m) >> 1) + 1;
-            odd = true;
-        } else {
-            target = (n + m) >> 1;
-            odd = false; 
-        }
-        cout << target << endl;
-        while (1) {
-            item1 = r1 != nums1.end() ? *r1 : magic_num;
-            item2 = r2 != nums2.end() ? *r2 : magic_num;
-            if (itr == target - 1) {
-                if (odd) {
-                    return min (item1, item2);
+        int mid = ((m + n) >> 1) + 1, i = 0, j = 0;
+
+        while (mid) {
+            if (i < m && j < n) {
+                if (nums1[i] <= nums2[j]) {
+                    pq.push(nums1[i++]);
                 } else {
-                    int a = min (item1, item2);
-                    if (item1 < item2)
-                        r1++;
-                    else
-                        r2++;
-                    item1 = r1 != nums1.end() ? *r1 : magic_num;
-                    item2 = r2 != nums2.end() ? *r2 : magic_num;
-                    int b = min (item1, item2);
-                    return (a + b)/2.0;
+                    pq.push(nums2[j++]);
                 }
+            } else if (i < m) {
+                pq.push(nums1[i++]);
             } else {
-                if (item1 < item2)
-                    r1++;
-                else
-                    r2++;
-                itr++;
+                pq.push(nums2[j++]);
             }
+            mid--;
+        }
+        
+        if ((m + n) & 1 == 1) {
+            return pq.top();
+        } else {
+            int a = pq.top();
+            pq.pop();
+            return  (a + pq.top())/2.0;
         }
     }
 };
