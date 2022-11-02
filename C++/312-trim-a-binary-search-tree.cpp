ec4c0f7a9d8cab806eb8029c22fca485f2ddbb12
tamimcse <tamim@csebuet.org>
2022-11-01 22:04:20 -0700

    .

--- C++/312-trim-a-binary-search-tree.cpp
+++ C++/312-trim-a-binary-search-tree.cpp
@@ -1,3 +1,20 @@
 /*
 https://leetcode.com/problems/trim-a-binary-search-tree/
 */
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if (!root)
+            return NULL;
+        if (root->val >= low && root->val <= high) {
+            root->left = trimBST (root->left, low, high);
+            root->right = trimBST (root->right, low, high);
+            return root;
+        }
+        
+        TreeNode *left = trimBST (root->left, low, high);
+        if (left)
+            return left;
+        return trimBST (root->right, low, high);
+    }
+};
