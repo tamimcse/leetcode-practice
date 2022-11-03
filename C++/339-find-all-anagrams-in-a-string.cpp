4284363e7a9924df25cfa5b6f45821f1be6d60bc
tamimcse <tamim@csebuet.org>
2022-11-02 21:46:28 -0700

    .

--- C++/339-find-all-anagrams-in-a-string.cpp
+++ C++/339-find-all-anagrams-in-a-string.cpp
@@ -1,3 +1,33 @@
 /*
 https://leetcode.com/problems/find-all-anagrams-in-a-string/
 */
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        int m = s.length(), n = p.length(), start = 0, end = 0;
+        int hash[26] = {0};
+        vector<int> res;
+        
+        for (int i = 0; i < n; i++) {
+            hash[p[i] - 'a']++;
+        }
+        
+        while (end < m) {
+            if (hash[s[end] - 'a']) {
+                hash[s[end] - 'a']--;
+                if (end - start + 1 == n) {
+                    res.push_back(start);
+                    hash[s[start] - 'a']++;
+                    start++;
+                    end++;
+                } else {
+                    end++;    
+                }
+            } else {
+                hash[s[start] - 'a']++;
+                start++; 
+            }
+        }
+        return res;
+    }
+};
