4a50e686a59bb8aa26971289cc6283c995228353
tamimcse <tamim@csebuet.org>
2022-11-08 20:16:15 -0800

    .

--- C++/271-house-robber.cpp
+++ C++/271-house-robber.cpp
@@ -1,3 +1,20 @@
 /*
 https://leetcode.com/problems/house-robber/
 */
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n);
+        
+        if (n == 1)
+            return nums[0];
+        
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = max (dp[i-1], dp[i-2] + nums[i]);
+        }
+        return dp[n-1];
+    }
+};
