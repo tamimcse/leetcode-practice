10d50cfc43e78353152ad8ecb1b44128ca16153b
tamimcse <tamim@csebuet.org>
2022-11-09 20:33:50 -0800

    .

--- C++/270-max-stack.cpp
+++ C++/270-max-stack.cpp
@@ -1,3 +1,37 @@
 /*
 https://leetcode.com/problems/max-stack/
 */
+class MinStack {
+    stack<int> s1;
+    stack<int> s2;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if (s1.empty()) {
+            s1.push(val);
+            s2.push(val);
+        } else {
+            if (val <= s2.top()) {
+                s2.push(val);
+            }
+            s1.push(val);
+        }
+    }
+    
+    void pop() {
+        if (s1.top() == s2.top())
+            s2.pop();
+        s1.pop();
+    }
+    
+    int top() {
+        return s1.top();
+    }
+    
+    int getMin() {
+        return s2.top();
+    }
+};
