d3ff859b90b95b6edf9f4bb6fba23aa0223f33d0
tamimcse <tamim@csebuet.org>
2022-11-10 21:46:22 -0800

    ,

--- C++/247-wiggle-sort.cpp
+++ C++/247-wiggle-sort.cpp
@@ -1,3 +1,16 @@
 /*
 https://leetcode.com/problems/wiggle-sort/
 */
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        int n = nums.size();
+        bool up = true;
+        for (int i = 1; i < n; i++) {
+            if ((up && nums[i] < nums[i-1]) || (!up && nums[i] > nums[i-1])) {
+                swap(nums[i], nums[i-1]);
+            }
+            up = !up;
+        }
+    }
+};
