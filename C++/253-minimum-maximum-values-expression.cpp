5ea9a203a8b2e3f5e09eed7077e30a8794b527b9
tamimcse <tamim@csebuet.org>
2022-11-11 21:26:51 -0800

    .

--- C++/253-minimum-maximum-values-expression.cpp
+++ C++/253-minimum-maximum-values-expression.cpp
@@ -1,3 +1,17 @@
 /*
 https://www.geeksforgeeks.org/minimum-maximum-values-expression/
 */
+#include <iostream>
+#include <string>
+using std::cout;
+using std::string;
+using std::endl;
+
+int max_value(string s) {
+  return 1;
+}
+
+int main() {
+  string s = "1*3+3*5+5+5*6";
+  cout << max_value (s) << endl;
+}
