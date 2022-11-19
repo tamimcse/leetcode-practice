b46ec7e24f81c85ad367ef5a14de2b40c34c3b4f
tamimcse <tamim@csebuet.org>
2022-11-18 18:46:13 -0800

    .

--- C++/244-insertion-sort-list.cpp
+++ C++/244-insertion-sort-list.cpp
@@ -1,3 +1,44 @@
 /*
 https://leetcode.com/problems/insertion-sort-list/
 */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode *r1 = head, *r2 = head->next, *r, *to_insert;
+        
+        while (r2) {
+            if (r2->val < head->val) {
+                to_insert = r2;
+                //delete the node
+                r1->next = r2->next;
+                r2 = r1->next;
+                //insert the node
+                to_insert->next = head;
+                head = to_insert;
+                continue;
+            }
+                
+            r = head;
+            bool found = false;
+            while (r->next != r2) {
+                if (r2->val < r->next->val) {
+                    to_insert = r2;
+                    //delete the node
+                    r1->next = r2->next;
+                    r2 = r1->next;
+                    //insert the node
+                    to_insert->next = r->next;
+                    r->next = to_insert;
+                    found = true;
+                    break;
+                }
+                r = r->next;
+            }
+            if (!found) {
+                r1 = r1->next;
+                r2 = r2->next;
+            }
+        }
+        return head;
+    }
+};
