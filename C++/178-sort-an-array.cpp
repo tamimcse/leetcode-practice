6efb0698aa9860837a4dda0ba88a7de260fdb6fb
tamimcse <tamim@csebuet.org>
2022-11-19 16:52:52 -0800

    .

--- C++/178-sort-an-array.cpp
+++ C++/178-sort-an-array.cpp
@@ -48,6 +48,42 @@ public:
         if (is_sorted(nums))
             return nums;
         sortArray (nums, 0, nums.size() - 1);
+        return nums;      
+    }
+};
+
+
+///////Merge sort
+class Solution {
+    void sortArray(vector<int>& nums, int start, int end) {
+        if (start + 1 == end) {
+            if (nums[start] > nums[end])
+                swap (nums[start], nums[end]);
+            return;
+        }
+        int mid = (start + end) >> 1;
+        if (mid > start)
+            sortArray (nums, start, mid);
+        if (end > mid + 1)
+            sortArray (nums, mid + 1, end);
+        //merge without using extra space
+        for (int i = end; i >= mid + 1; i--) {
+            if (nums[i] < nums[mid]) {
+                int to_insert = nums[i];
+                nums[i] = nums[mid];
+                int j = mid - 1;
+                while (j >= start && nums[j] > to_insert) {
+                    nums[j+1] = nums[j];
+                    j--;
+                }
+                nums[j+1] = to_insert;
+            }
+        }
+    }
+    
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        sortArray (nums, 0, nums.size() - 1);
         return nums;
     }
 };
