2a5561220faf2b1e2e45f17b626fa4bdf41c31a1
tamimcse <tamim@csebuet.org>
2022-11-20 16:37:18 -0800

    ,

--- C++/274-longest-common-subsequence.cpp
+++ C++/274-longest-common-subsequence.cpp
@@ -1,3 +1,27 @@
 /*
 https://leetcode.com/problems/longest-common-subsequence/
 */
+class Solution {
+    bool isPalindrome(string s) {
+        int i = 0, j = s.length() - 1;
+        
+        for (; i < j; i++, j--) {
+            if (s[i] != s[j])
+                return false;
+        }
+        return true;
+    }
+
+public:
+    int countSubstrings(string s) {
+        int count = 0, n = s.length();
+                
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (isPalindrome (s.substr(i, j - i + 1)))
+                    count++;
+            }
+        }
+        return count;
+    }
+};
