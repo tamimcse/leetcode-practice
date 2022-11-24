f2b193997c4e7c03de84234b70fe5ceb5202d2de
tamimcse <tamim@csebuet.org>
2022-11-23 21:41:18 -0800

    .

--- C++/324-next-permutation.cpp
+++ C++/324-next-permutation.cpp
@@ -1,3 +1,20 @@
 /*
 https://leetcode.com/problems/next-permutation/
 */
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = n - 1; j > i; j--) {
+                if (nums[j] > nums[i]) {
+                    swap (nums[i], nums[j]);
+                    sort(nums.begin() + i + 1, nums.end());
+                    return;
+                }
+            }
+        }
+        sort(nums.begin(), nums.end());
+    }
+};
