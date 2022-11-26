f5836da5ce15b6e57ecf8ad8b65129c649be036a
tamimcse <tamim@csebuet.org>
2022-11-25 20:24:03 -0800

    .

--- C++/348-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ C++/348-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,3 +1,33 @@
 /*
 https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/
 */
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        stack<pair<char, int>> stack;
+        int n = s.length();
+        
+        for (int i = 0; i < n; i++) {
+            if(!stack.empty() && stack.top().first == s[i]) {
+                stack.top().second++;
+                if (stack.top().second == k) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(make_pair(s[i], 1));
+            }
+        }
+        
+        string res;
+        while (!stack.empty()) {
+            auto cur = stack.top();
+            stack.pop();
+            for (int i = 0; i < cur.second; i++) {
+                string tmp;
+                tmp += cur.first;
+                res.insert(0, tmp);
+            }
+        }
+        return res;
+    }
+};
