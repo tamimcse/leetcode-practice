b864f41edce65f770c57c99bb4ef1c3faad39d8b
tamimcse <tamim@csebuet.org>
2022-11-28 15:28:05 -0800

    .

--- C++/314-binary-tree-right-side-view.cpp
+++ C++/314-binary-tree-right-side-view.cpp
@@ -1,3 +1,32 @@
 /*
 https://leetcode.com/problems/binary-tree-right-side-view/
 */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        queue<pair<TreeNode*, int>> q;
+        vector<int> res;
+        int level;
+        TreeNode* n;
+        int prev_level = -1;
+        
+        if (!root)
+            return res;
+        
+        q.push(make_pair(root, 0));
+        while (!q.empty()) {
+            n = q.front().first;
+            level = q.front().second;
+            q.pop();
+            if (level != prev_level) {
+                res.push_back(n->val);
+                prev_level = level;
+            }
+            if (n->right)
+                q.push(make_pair(n->right, level + 1));
+            if (n->left)
+                q.push(make_pair(n->left, level + 1));
+        }
+        return res;
+    }
+};
