a8bf58a1b7804fbee8ec607eb20b74e5ee54bac7
tamimcse <tamim@csebuet.org>
2022-11-27 18:03:08 -0800

    .

--- C++/265-flip-equivalent-binary-trees.cpp
+++ C++/265-flip-equivalent-binary-trees.cpp
@@ -1,3 +1,40 @@
 /*
 https://leetcode.com/problems/flip-equivalent-binary-trees/
 */
+class Solution {
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        if (!root1 && !root2)
+            return true;
+        if ((!root1 && root2) || (root1 && !root2))
+            return false;
+        if (root1->val != root2->val)
+            return false;
+        
+        int num_child1 = 0, num_child2 = 0;
+        if (root1->left)
+            num_child1++;
+        if (root1->right)
+            num_child1++;
+        if (root2->left)
+            num_child2++;
+        if (root2->right)
+            num_child2++;
+        
+        if (num_child1 != num_child2)
+            return false;
+        
+        if (root1->left == NULL && root1->right == NULL &&
+           root2->left == NULL && root2->right == NULL)
+            return true;
+        bool ret1 = flipEquiv(root1->left, root2->left);
+        bool ret2 = flipEquiv(root1->right, root2->right);
+        
+        if (ret1 && ret2)
+            return true;
+        
+        ret1 = flipEquiv(root1->left, root2->right);
+        ret2 = flipEquiv(root1->right, root2->left);
+        return ret1 && ret2;
+    }
+};
