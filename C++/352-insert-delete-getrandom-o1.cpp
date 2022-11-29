0b57bd797ee94b4490532d4dcfc0c714800c57fb
tamimcse <tamim@csebuet.org>
2022-11-28 21:31:41 -0800

    .

--- C++/352-insert-delete-getrandom-o1.cpp
+++ C++/352-insert-delete-getrandom-o1.cpp
@@ -1,3 +1,35 @@
 /*
 https://leetcode.com/problems/insert-delete-getrandom-o1/
 */
+class RandomizedSet {
+    unordered_set<int> us;
+public:
+    RandomizedSet() {
+        srand(time(nullptr));
+    }
+    
+    bool insert(int val) {
+        if (us.count(val) == 0) {
+            us.insert(val);
+            return true;
+        }
+        return false;
+    }
+    
+    bool remove(int val) {
+        if (us.count(val)) {
+            us.erase(val);
+            return true;
+        }
+        return false;
+    }
+    
+    int getRandom() {
+        int size = us.size();
+        int idx = rand() % size;
+        auto it = us.begin();
+        advance(it, idx);
+        return *it;
+    }
+};
+
