e76e1beef67e3c47e8b34e363e245c95f4644c48
tamimcse <tamim@csebuet.org>
2022-11-30 21:39:47 -0800

    .

--- C++/359-lonely-pixel-i.cpp
+++ C++/359-lonely-pixel-i.cpp
@@ -1,3 +1,40 @@
 /*
 https://leetcode.com/problems/lonely-pixel-i/
 */
+class Solution {
+    
+public:
+    int findLonelyPixel(vector<vector<char>>& picture) {
+        int m = picture.size(), n = picture[0].size();
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (picture[i][j] == 'B') {
+                    for (int p = 0; p < m; p++) {
+                        if (i != p && 
+                            (picture[p][j] == 'B' || picture[p][j] == 'A')) {
+                            picture[i][j] = 'A';
+                            picture[p][j] = 'A';
+                        }
+                    }
+                    for (int p = 0; p < n; p++) {
+                        if (j != p && (picture[i][p] == 'B' || picture[i][p] == 'A')) {
+                            picture[i][j] = 'A';
+                            picture[i][p] = 'A';
+                        }
+                    }
+                }
+            }
+        }
+        
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (picture[i][j] == 'B') {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
