dbebd0ba33ab7d7661c2d0a39b9af9c985676f85
tamimcse <tamim@csebuet.org>
2022-12-05 18:58:53 -0800

    .

--- C++/370-serialize-and-deserialize-bst.cpp
+++ C++/370-serialize-and-deserialize-bst.cpp
@@ -1,5 +1,7 @@
 /*
 https://leetcode.com/problems/serialize-and-deserialize-bst/
 
-This is same as creating binary tree from inorder + preorder except we don't need the inorder. In-order in BST will always be sorted. So, we can use pre-order to split into left and right subtree
+This is same as creating binary tree from inorder + preorder except we don't need the inorder. In-order in BST will always be sorted. So, we can use pre-order to split into left and right subtree.
+
+I'm skipping the implementation for now
 */
