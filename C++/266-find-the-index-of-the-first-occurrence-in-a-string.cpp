0990791dfe5e0a442409f85a51059cc8e628506e
tamimcse <tamim@csebuet.org>
2022-12-15 18:37:24 -0800

    .

--- C++/266-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ C++/266-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,27 +1,22 @@
 /*
 https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 
-Implement both naive solution and KMP
+Implement both naive solution and KMP. Note that, naive solution is also accepted by leetcode
 */
 class Solution {
 public:
     int strStr(string haystack, string needle) {
         int m = haystack.length(), n = needle.length();
 
-        for (int i = 0; i < m - n + 1;) {
-            if (haystack[i] != needle[0]) {
-                i++;
-                continue;
-            }
-            for (int j = 0; j < n; j++) {
-                if (haystack[i + j] == needle[j]) {
-                    if (j == n - 1)
-                        return i;
-                } else {
-                    i++;
-                    while (i < m && haystack[i] != needle[0])
-                        i++;
-                    break;
+        for (int i = 0; i < m - n + 1; i++) {
+            if (haystack[i] == needle[0]) {
+                for (int j = 0; j < n; j++) {
+                    if (haystack[i + j] == needle[j]) {
+                        if (j == n - 1)
+                            return i;
+                    } else {
+                        break;
+                    }
                 }
             }
         }
