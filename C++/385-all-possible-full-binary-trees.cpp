4f97ad2a6903afcab2296575d5f4b7d0969280ae
tamimcse <tamim@csebuet.org>
2022-12-18 04:17:19 -0800

    .

--- C++/385-all-possible-full-binary-trees.cpp
+++ C++/385-all-possible-full-binary-trees.cpp
@@ -1,3 +1,33 @@
 /*
 https://leetcode.com/problems/all-possible-full-binary-trees/
 */
+class Solution {
+
+public:
+    vector<TreeNode*> allPossibleFBT(int n) {
+        vector<TreeNode*> res;
+
+        if (n % 2 == 0)
+            return res;
+
+        if (n == 1) {
+            TreeNode *root = new TreeNode();
+            res.push_back(root);
+            return res;    
+        }
+
+        for (int i = 1; i < n - 1; i += 2) {
+            vector<TreeNode*> leftSubtrees = allPossibleFBT(i);
+            vector<TreeNode*> rightSubtrees = allPossibleFBT(n - 1 - i);
+            for (auto t1 : leftSubtrees) {
+                for (auto t2 : rightSubtrees) {
+                    TreeNode *root = new TreeNode();
+                    root->left = t1;
+                    root->right = t2;
+                    res.push_back(root);
+                }
+            }
+        }
+        return res;
+    }
+};
