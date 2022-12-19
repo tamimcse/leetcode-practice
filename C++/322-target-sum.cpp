71f162fa668b2f400d28895096324065188d185f
tamimcse <tamim@csebuet.org>
2022-12-19 15:14:57 -0800

    .

--- C++/322-target-sum.cpp
+++ C++/322-target-sum.cpp
@@ -1,3 +1,37 @@
 /*
 https://leetcode.com/problems/target-sum/
 */
+
+/********This gets TLE*************/
+class Solution {
+    int calc_sum (vector<int>& nums, vector<char>& ops) {
+        int n = nums.size();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += ops[i] == '+' ? nums[i] : -nums[i];
+        }
+        return sum;
+    }
+    void findTargetSumWays(vector<int>& nums, int target,
+                        vector<char>& ops, int idx, int &count) {
+        int n = nums.size();
+        if (idx == n) {
+            if (calc_sum (nums, ops) == target)
+                count++;
+            return;
+        }
+        ops[idx] = '+';
+        findTargetSumWays (nums, target, ops, idx + 1, count);
+        ops[idx] = '-';
+        findTargetSumWays (nums, target, ops, idx + 1, count);
+    }
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        vector<char> ops(nums.size());
+        int count = 0;
+
+        findTargetSumWays (nums, target, ops, 0, count);
+        return count;
+    }
+};
