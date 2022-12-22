de0af805c69324e1edf996d389120b2e932457c2
tamimcse <tamim@csebuet.org>
2022-12-21 20:55:41 -0800

    .

--- C++/334-subarray-sum-equals-k.cpp
+++ C++/334-subarray-sum-equals-k.cpp
@@ -1,3 +1,32 @@
 /*
 https://leetcode.com/problems/subarray-sum-equals-k/
+
+Very interesting problem!!!!
 */
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> prefixSum(n);
+        int count = 0;
+        unordered_map<int, int> hash;
+
+        prefixSum[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (prefixSum[i] == k)
+                count++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            auto it = hash.find(prefixSum[i] - k);
+            if (it != hash.end())
+                count += it->second;
+            hash[prefixSum[i]]++;    
+        }
+        return count;
+    }
+};
