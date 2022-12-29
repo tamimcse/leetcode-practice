a9ce5621af3a6ef3ad4e68e4d9aa66af02bfd7d1
tamimcse <tamim@csebuet.org>
2022-12-28 21:09:22 -0800

    .

--- C++/285-word-ladder-ii.cpp
+++ C++/285-word-ladder-ii.cpp
@@ -3,10 +3,6 @@ https://leetcode.com/problems/word-ladder-ii
 
 very interesting problem!!!
 */
-
-
-/*****This solution gets TLE*************************************************/
-
 class Solution {
     bool is_adjacent (string &s1, string &s2) {
         int n = s1.length();
@@ -24,163 +20,72 @@ class Solution {
         }
         return true;
     }
-public:
-    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        vector<vector<string>> res;
-        int start = -1, end;
-        int max_depth = -1;
 
-        for (int i = 0; i < wordList.size(); i++) {
-            if (wordList[i] == beginWord)
-                start = i;
-            if (wordList[i] == endWord)
-                end = i;
+    void add_edges (string dest, vector<string>& wordList,
+                    unordered_set<string> &visit,
+                    unordered_map<string, vector<string>> &adj_list) {
+        for (auto &e : wordList) {
+            if (e != dest && visit.count(e) == 0 && is_adjacent(e, dest))
+                adj_list[e].push_back(dest);
         }
-        if (start == -1) {
-            wordList.push_back(beginWord);
-            start = wordList.size() - 1;
-        }
-
-        int n = wordList.size();
-
-        vector<vector<int>> adj_list (n);
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (is_adjacent (wordList[i], wordList[j])) {
-                    adj_list[i].push_back(j);
-                    adj_list[j].push_back(i);
-                }
-            }
-        }
-
-        queue<pair<int, int>> q;
-        vector<int> visit(n);
-        queue<vector<int>> res_q;
-
-        q.push(make_pair(start, 1));
-        res_q.push({start});
-        while (!q.empty()) {
-            int cur_word = q.front().first;
-            int level = q.front().second;
-            vector<int> cur_vec = res_q.front();
-            q.pop();
-            res_q.pop();
-            visit[cur_word] = true;
-            for (int i : adj_list[cur_word]) {
-                if (max_depth > 0 && level > max_depth)
-                    return res;
-                if (visit[i])
-                    continue;    
-
-                if (i == end) {
-                    vector<string> tmp;
-                    for (int e : cur_vec) {
-                        tmp.push_back(wordList[e]);
-                    }
-                    tmp.push_back(endWord);
-                    res.push_back(tmp);
-                    max_depth = level;
-                    continue;
-                }
-                q.push(make_pair(i, level + 1));
-                vector<int> tmp = cur_vec;
-                tmp.push_back(i);
-                res_q.push(tmp);
-            }
-        }
-
-        return  res;
-    }
-};
-
-
-/**********This also gets TLE******************/
-class Solution {
-    unordered_map<string, vector<string>> adj_list;
-    vector<string> path;
-
-    bool is_adjacent (string &s1, string &s2) {
-        int n = s1.length();
-        int count = 0;
-
-        if (s1 == s2)
-            return false;
-
-        for (int i = 0; i < n; i++) {
-            if (s1[i] != s2[i]) {
-                count++;
-                if (count > 1)
-                    return false;
-            }
-        }
-        return true;
-    }
-    vector<string> findNeighbors(vector<string> &list, unordered_set<string> &visit,
-                                string node) {
-        vector<string> res;
-        for (int i = 0; i < list.size(); i++) {
-            if (visit.count(list[i]) == 0 && is_adjacent(list[i], node))
-                res.push_back(list[i]);
-        }
-        return res;
     }
 
-    void dfs (string cur, string end, vector<vector<string>> &res) {
-        cout << cur << " ";
+    void dfs (string cur, unordered_map<string, vector<string>> &adj_list,
+              vector<string> &path, vector<vector<string>> &res, string &end) {
         path.push_back(cur);
         if (cur == end) {
-            reverse(path.begin(), path.end());
-            res.push_back(path);
-            reverse(path.begin(), path.end());
+            vector<string> cur_path = path;
+            reverse(cur_path.begin(), cur_path.end());
+            res.push_back(cur_path);
             path.pop_back();
             return;
         }
-
         for (auto &e : adj_list[cur]) {
-            dfs (e, end, res);
+            dfs (e, adj_list, path, res, end);
         }
         path.pop_back();
     }
 public:
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        if (find(wordList.begin(), wordList.end(), beginWord) == wordList.end())
-            wordList.push_back(beginWord);
-        queue<pair<string, int>> q;
-        string cur_str;
-        int cur_lev;
+        unordered_map<string, vector<string>> adj_list;
+        queue<string> q;
         unordered_set<string> visit;
-        
-        int max_depth = -1;
-        q.push(make_pair(beginWord, 1));
+        string cur;
+
+        q.push(beginWord);
         while (!q.empty()) {
-            cur_str = q.front().first;
-            cur_lev = q.front().second;
+            cur = q.front();
             q.pop();
-            if (visit.count(cur_str))
-                continue;
-            vector<string> neigh = findNeighbors (wordList, visit, cur_str);
-            for (auto e : neigh)
-                adj_list[e].push_back(cur_str); 
-            visit.insert(cur_str);
-            cout << cur_str << endl;
-            bool found_end = false;
-            for (auto &e : neigh) {
+            visit.insert(cur);
+            //cout << cur << " ";
+            for (auto &e : wordList) {
+                if (visit.count(e) || !is_adjacent(e, cur))
+                    continue;    
+                adj_list[e].push_back(cur);
+                cout << "Adding edge between " << e << " and " << cur << endl;
+                q.push(e);
                 if (e == endWord) {
-                    found_end = false;
-                    break;
+                    goto backtrack;
                 }
-                cout << "Pushing " << e << endl;        
-                q.push(make_pair(e, cur_lev + 1));
             }
-            if (found_end)
-                break;
         }
 
-        vector<vector<string>> res;
-        
-        cout << "DFS = ";
+backtrack:
 
-        dfs (endWord, beginWord, res);
-        return  res;
+        while (!q.empty()) {
+            cur = q.front();
+            q.pop();
+            if (visit.count(cur) == 0 && is_adjacent(endWord, cur)) {
+                adj_list[endWord].push_back(cur);
+                visit.insert(cur);
+                cout << "Adding edge between " << endWord << " and " << cur << endl;
+            }
+        }
+        
+        vector<vector<string>> res;
+        vector<string> path;
+
+        dfs (endWord, adj_list, path, res, beginWord);
+        return res;
     }
 };
