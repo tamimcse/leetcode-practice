1b340a1393b81df66a8c4c3af551ce0e9ed18ccf
tamimcse <tamim@csebuet.org>
2022-12-29 21:52:16 -0800

    ,

--- C++/326-perfect-squares.cpp
+++ C++/326-perfect-squares.cpp
@@ -1,3 +1,32 @@
 /*
 https://leetcode.com/problems/perfect-squares/
 */
+class Solution {
+    bool isperfectSqr (int n) {
+        int s = sqrt(n);
+
+        return s * s == n;
+    }
+public:
+    int numSquares(int n) {
+        if (isperfectSqr(n))
+            return 1;
+
+        vector<int> dp(n+1);
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            if (isperfectSqr(i)) {
+                dp[i] = 1;
+                cout << dp[i] << " ";
+                continue;
+            }
+            int min_options = INT_MAX;
+            int max_sqrt = sqrt (i);
+            for (int j = max_sqrt; j >= 1; j--) {
+                min_options = min (min_options, dp[j*j] + dp[i-j*j]);
+            }
+            dp[i] = min_options;
+        }
+        return dp[n];
+    }
+};
