0573119caf254640996db11073d12b5da9212556
tamimcse <tamim@csebuet.org>
2022-12-29 18:48:19 -0800

    .

--- C++/199-encode-and-decode-strings.cpp
+++ C++/199-encode-and-decode-strings.cpp
@@ -1,5 +1,10 @@
 /*
 https://leetcode.com/problems/encode-and-decode-strings/
+
+Implement both approach:
+
+1. Using delemeter 
+2. Generic approach where it works for any possible characters
 */
 class Codec {
 public:
@@ -30,6 +35,47 @@ public:
                 res.push_back(s.substr(start, end - start));
             start = end + 1;    
         } while (end != string::npos);
+        return res;
+    }
+};
+
+
+class Codec {
+    string encode_len(string s) {
+        string res = to_string(s.length());
+
+        while (res.length() < 3) {
+            res = "0" + res;
+        }
+        return res;
+    }
+
+public:
+
+    // Encodes a list of strings to a single string.
+    string encode(vector<string>& strs) {
+        string res;
+        
+        for (int i = 0; i < strs.size(); i++) {
+            res += encode_len(strs[i]);
+            res += strs[i];
+        }
+        return res;
+    }
+
+    // Decodes a single string to a list of strings.
+    vector<string> decode(string s) {
+        vector<string> res;
+        int start = 0;
+        int end;
+        int len;
+
+        while (start < s.length()) {
+            len = atoi(s.substr(start, 3).c_str());
+            start += 3;
+            res.push_back(s.substr(start, len));
+            start += len;
+        }
         return res;
     }
 };
