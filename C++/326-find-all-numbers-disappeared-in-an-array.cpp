f2e6ae96dc6735e1536ff34fd40af09067fd7ec8
tamimcse <tamim@csebuet.org>
2023-01-07 18:16:00 -0800

    .

--- C++/326-find-all-numbers-disappeared-in-an-array.cpp
+++ C++/326-find-all-numbers-disappeared-in-an-array.cpp
@@ -21,3 +21,23 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res;
+
+        for (int i = 0; i < n; i++) {
+            int num = abs(nums[i]) - 1;
+            if (nums[num] > 0)
+                nums[num] = -nums[num];
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0)
+                res.push_back(i+1);
+        }
+        return res;
+    }
+};
