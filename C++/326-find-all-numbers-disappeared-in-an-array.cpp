4fefd91c216a6651eda8fbdeb8238e96e6a5f10e
tamimcse <tamim@csebuet.org>
2023-01-07 18:05:42 -0800

    .

--- C++/326-find-all-numbers-disappeared-in-an-array.cpp
+++ C++/326-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,3 +1,23 @@
 /*
 https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+
+Implement using both extra space and O(1) space
 */
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res;
+        vector<int> freq(n+1);
+
+        for (auto i : nums) {
+            freq[i]++;
+        }
+
+        for (int i = 1; i < n+1; i++) {
+            if (!freq[i])
+                res.push_back(i);
+        }
+        return res;
+    }
+};
