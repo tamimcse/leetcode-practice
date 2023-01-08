cf9a14327bf49b2a0dad68879e090813ac77dd11
tamimcse <tamim@csebuet.org>
2023-01-07 21:19:35 -0800

    .

--- C++/335-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
+++ C++/335-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -1,3 +1,5 @@
 /*
 https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/
+
+Very interesting problem. The implementation has some complicacy. So, try to implemement again.
 */
