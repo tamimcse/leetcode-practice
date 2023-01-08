c7d34cf56d58030fc826115ce9d38d9da5cf47a8
tamimcse <tamim@csebuet.org>
2023-01-07 18:39:25 -0800

    .

--- C++/328-swap-nodes-in-pairs.cpp
+++ C++/328-swap-nodes-in-pairs.cpp
@@ -1,3 +1,16 @@
 /*
 https://leetcode.com/problems/swap-nodes-in-pairs/
 */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (!head || !head->next)
+            return head;
+        ListNode *r1 = head, *r2 = head->next;
+
+        ListNode *r3 = swapPairs (r2->next);
+        r2->next = r1;
+        r1->next = r3;
+        return r2;
+    }
+};
