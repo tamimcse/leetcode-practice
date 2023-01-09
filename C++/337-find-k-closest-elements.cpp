5716bfe307c03b3bc61ce78739e02203cc6a2d27
tamimcse <tamim@csebuet.org>
2023-01-08 19:53:48 -0800

    .

--- C++/337-find-k-closest-elements.cpp
+++ C++/337-find-k-closest-elements.cpp
@@ -3,3 +3,17 @@ https://leetcode.com/problems/find-k-closest-elements/
 
 Interesting problem
 */
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        sort (arr.begin(), arr.end(), [&](int a, int b) {
+            if (abs(x - a) == abs(x - b))
+                return a < b;
+            return abs(x - a) < abs(x - b);
+        });
+
+        vector<int> res (arr.begin(), arr.begin() + k);
+        sort (res.begin(), res.end());
+        return res;        
+    }
+};
