20915ee04664b2907cdef26eaf20d69865630578
tamimcse <tamim@csebuet.org>
2023-01-08 19:24:17 -0800

    .

--- C++/322-remove-k-digits.cpp
+++ C++/322-remove-k-digits.cpp
@@ -1,3 +1,27 @@
 /*
 https://leetcode.com/problems/remove-k-digits/
 */
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        for (int i = 0; i < k; i++) {
+            while (num.length() > 1 && num[0] == '0')
+                num.erase(0, 1);
+            int n = num.length();
+            if (n <= k - i)
+                return "0";
+            for (int j = 1; j < n; j++) {
+                if (num[j] < num[j-1]) {
+                    num.erase(j-1, 1);
+                    break;
+                } else if (j == n - 1) {
+                    num.erase(n - (k - i));
+                    return num;
+                }
+            }
+        }
+        while (num.length() > 1 && num[0] == '0')
+            num.erase(0, 1);
+        return num;
+    }
+};
