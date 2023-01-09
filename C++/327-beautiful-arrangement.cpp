6b963b1bb89f7483d5a225411f0873877187d7bd
tamimcse <tamim@csebuet.org>
2023-01-08 16:03:09 -0800

    .

--- C++/327-beautiful-arrangement.cpp
+++ C++/327-beautiful-arrangement.cpp
@@ -1,3 +1,40 @@
 /*
 https://leetcode.com/problems/beautiful-arrangement/
+
+Very common problem in the interview!!!
 */
+
+/***This gets TLE**********************************/
+class Solution {
+    bool isBeautiful (int idx, int val) {
+        return val % idx == 0 || idx % val == 0;
+    }
+public:
+    int countArrangement(int n) {
+        vector<vector<unordered_set<int>>> dp(n+1);
+
+        vector<unordered_set<int>> v;
+        for (int i = 1; i <= n; i++) {
+            if (isBeautiful (i, n)) {
+                v.push_back({i});
+            }
+        }
+        dp[n] = v;
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n; j++) {
+                if (isBeautiful (j, i)) {
+                    for (auto e : dp[i+1]) {
+                        if (e.count(j) == 0) {
+                            unordered_set<int> s = e;
+                            s.insert(j);
+                            dp[i].push_back(s);
+                        }
+                    }
+                }
+            }
+        }
+        return dp[1].size();
+    }
+};
+
+
