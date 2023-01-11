10f7fd13a76219c4b3f5f8c8d8d98eeefa5c8c35
tamimcse <tamim@csebuet.org>
2023-01-10 19:36:21 -0800

    ,

--- C++/336-maximum-xor-of-two-numbers-in-an-array.cpp
+++ C++/336-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -3,5 +3,5 @@ https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/
 
 Very very very very interesting problem!!
 
-Implement both using hash and trie
+Implement both using hash and trie. When implementing hash based approch, I learned some cool thing, but eventuall decided not to use that approch. I only used trie based approach. 
 */
