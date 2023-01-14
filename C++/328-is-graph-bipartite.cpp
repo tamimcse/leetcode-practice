2f52cd589d715ac968a5c2bb66cd6c171c3f7a9e
tamimcse <tamim@csebuet.org>
2023-01-13 21:12:09 -0800

    .

--- C++/328-is-graph-bipartite.cpp
+++ C++/328-is-graph-bipartite.cpp
@@ -3,3 +3,32 @@ https://leetcode.com/problems/is-graph-bipartite/
 
 Very very very interesting problem!!!!!
 */
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        queue<int> q;
+        vector<int> color(n);
+
+        for (int i = 0; i < n; i++) {
+            if (color[i])
+                continue;
+            q.push(i);
+            color[i] = 1;
+            while (!q.empty()) {
+                int node = q.front();
+                q.pop();
+                for (int adj : graph[node]) {
+                    if (color[adj] != 0 && 
+                        color[adj] == color[node])
+                        return false;
+                    if (color[adj] == 0) {
+                        q.push(adj);
+                        color[adj] = color[node] == 1 ? 2 : 1;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
