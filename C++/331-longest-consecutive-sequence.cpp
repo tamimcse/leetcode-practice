bed413a01ad7529f113476814aaaf04fe62863b6
tamimcse <tamim@csebuet.org>
2023-01-14 20:28:11 -0800

    .

--- C++/331-longest-consecutive-sequence.cpp
+++ C++/331-longest-consecutive-sequence.cpp
@@ -7,10 +7,45 @@ Problem isn't very clear. What does it mean by sequnce? Here, they mean longest 
 
 Sometimes, you may come across a problem where you need to check if you came through this path. This problem particularly arise in sequence building. In that, check if you can solve this using DFS/BFS. Usually you can. An example of such problem is longest-consecutive-sequence.
 
-But, note that, when you really want to create a sequence, do you really need to start from the any node. For instance, in longest-consecutive-sequence problem, you are given an array. The first element of the array may not be the first element of the sequence. This is why, you may need to look both ways to create sequence. Solving this problem with DFS and BFS also involve havinhg visit array so that we don't visit an element if we visited before. But, all this problem can be simplified if we only start with elements that's the start of a sequence. In that case, we don't need to look bi-directionally. We only look unidirectionally. For instance, Approach #3
+But, note that, here node node will have just one successor. Do you really need traditional recursive or stack based DFS here? No, you can implemement the DFS by a simple loop!!!!!!
+Implementing stack based DFS results in TLE 
 
 https://leetcode.com/problems/longest-consecutive-sequence/solution/
-
-
-But, implememnt it using both DFS and better approch. DFS will get TLE though.
 */
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, bool> hash;
+        unordered_set<int> not_start_nodes;
+
+        for (auto e : nums) {
+            if (hash.find(e + 1) != hash.end()) {
+                hash[e] = true;
+                not_start_nodes.insert(e + 1);
+            }
+            if (hash.find(e - 1) != hash.end()) {
+                hash[e-1] = true;
+                not_start_nodes.insert(e);
+            }
+            if (hash.find(e) == hash.end()) {
+                hash[e] = false;
+            }
+        }
+
+        int res = 0;
+        for (auto e : nums) {
+            if (not_start_nodes.count(e) == 0) {
+                int num = e;
+                int count = 1;
+                while (hash[num] == true) {
+                    num++;
+                    count++;
+                }
+                res = max (res, count);
+            }
+        }
+
+        return res;
+
+    }
+};
