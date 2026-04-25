2e6f4eee1dc8473ffbfcf63eb62b0b710bc4f493
tamimcse <mislam4@kent.edu>
2026-04-25 15:21:51 -0700

    .

--- C++/162-daily-temperatures.cpp
+++ C++/162-daily-temperatures.cpp
@@ -27,3 +27,27 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> res(temperatures.size());
+
+        res[res.size() - 1] = 0;
+        int maxIdx = res.size() - 1;
+        for (int i = res.size() - 2; i >= 0; i--) {
+            if (temperatures[i] < temperatures[i+1]) {
+                res[i] = 1;
+                maxIdx = i;
+            } else {
+                for (int j = maxIdx; j < res.size(); j++) {
+                    if (temperatures[j] > temperatures[i]) {
+                        res[i] = j - i;
+                        break;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
