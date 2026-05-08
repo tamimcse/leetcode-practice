0caddd120f7ed1739e7a77430343ae594d3c79e1
tamimcse <mislam4@kent.edu>
2026-05-08 12:53:11 -0700

    .

--- C++/169-design-hit-counter.cpp
+++ C++/169-design-hit-counter.cpp
@@ -4,20 +4,38 @@ https://leetcode.com/problems/design-hit-counter/
 How can you make it scale such that it supports large number of hits per second? I know the way. Seem too trivial to implement. But, make sure you know it.
 */
 class HitCounter {
-    queue<int> q;
+    map<unsigned int, unsigned int> hitCountMap;
+    int count = 0;
 public:
     HitCounter() {
         
     }
     
     void hit(int timestamp) {
-        q.push(timestamp);
+        if (hitCountMap.empty() || ((timestamp - hitCountMap.begin()->first) < 300)) {
+            hitCountMap[timestamp]++;
+            count++;
+        } else {
+            for (auto it = hitCountMap.begin(); it != hitCountMap.end();) {
+                if ((timestamp - it->first) < 300)
+                    break;
+                count -= it->second;
+                it = hitCountMap.erase(it);
+            }
+            hitCountMap[timestamp]++;
+            count++;
+        }
     }
     
     int getHits(int timestamp) {
-        while (!q.empty() && (timestamp - q.front() >= 300)) {
-            q.pop();
+        if (hitCountMap.empty() || ((timestamp - hitCountMap.begin()->first) < 300))
+            return count;
+        for (auto it = hitCountMap.begin(); it != hitCountMap.end();) {
+            if ((timestamp - it->first) < 300)
+                break;
+            count -= it->second;
+            it = hitCountMap.erase(it);
         }
-        return q.size();
+        return count;
     }
 };
